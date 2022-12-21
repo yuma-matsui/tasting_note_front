101e55717e18a78f17c11441f95ea39ffbc84630
yuma-matsui <oshino.iiyo@gmail.com>
2022-12-21 20:15:27 +0900

    フッターにトップページへのリンクを追加した

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,12 +1,15 @@
 import { FC } from 'react'
+import { Link } from 'react-router-dom'
 import logo from '../../assets/images/demo_logo.jpg'
 
 const Footer: FC = () => (
   <footer style={{ borderTop: '1px solid gray', margin: '32px auto', paddingTop: '16px' }}>
-    <div style={{ display: 'flex' }}>
-      <img src={logo} alt="Footer Logo" style={{ width: '50px', height: '50px' }} />
-      <p>Tasting Note</p>
-    </div>
+    <Link to="/" style={{ color: '#000', textDecoration: 'none' }}>
+      <div style={{ display: 'flex' }}>
+        <img src={logo} alt="Footer Logo" style={{ width: '50px', height: '50px' }} />
+        <p>Tasting Note</p>
+      </div>
+    </Link>
     <ul style={{ display: 'flex', listStyle: 'none' }}>
       <li style={{ marginRight: '16px' }}>利用規約</li>
       <li>プライバシーポリシー</li>
