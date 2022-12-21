816f7703c62c66c43984f22947ea24879ebc38d6
yuma-matsui <oshino.iiyo@gmail.com>
2022-12-21 20:16:25 +0900

    ヘッダーにトップページへのリンクを追加した

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,13 +1,18 @@
 import { FC } from 'react'
+import { Link } from 'react-router-dom'
 import logo from '../../assets/images/demo_logo.jpg'
 
 const Header: FC = () => (
-  <header style={{ display: 'flex', borderBottom: '1px solid red' }}>
-    <img src={logo} alt="Header Logo" style={{ width: '100px', height: '100px' }} />
-    <div>
-      <h2>Tasting Note</h2>
-      <p>テイスティングを記録してソムリエに</p>
-    </div>
+  <header>
+    <Link to="/" style={{ textDecoration: 'none', color: '#000' }}>
+      <div style={{ display: 'flex', borderBottom: '1px solid red' }}>
+        <img src={logo} alt="Header Logo" style={{ width: '100px', height: '100px' }} />
+        <div>
+          <h2>Tasting Note</h2>
+          <p>テイスティングを記録してソムリエに</p>
+        </div>
+      </div>
+    </Link>
   </header>
 )
 
