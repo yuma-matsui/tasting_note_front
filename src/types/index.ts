93e2c7bbd69cc78472b1523b5fb21ba2a6d6dcec
yuma-matsui <oshino.iiyo@gmail.com>
2022-12-22 22:27:19 +0900

    テイスティングシートのwineColorカラム用の型を定義した

---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+export type { default as WineColor } from './wineColor'
+export type { default as TastingSheet } from './tastingSheet'
+export type { default as TastingSheetContextType } from './tastingSheetContextType'
+export type { default as ReactNodeChildren } from './reactNodeChildren'
