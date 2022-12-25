d456416676e1792d4f0600e8d1794230178f365b
yuma-matsui <oshino.iiyo@gmail.com>
2022-12-25 20:49:32 +0900

    コンポーネントでコレクションをループさせるために全てのコレクションを含む型を定義した

---
+++ src/types/appearance/shared/appearanceAllSelection.ts
@@ -0,0 +1,24 @@
+import AppearanceColorRed from '../red/appearanceColorRed'
+import AppearanceImpressionRed from '../red/appearanceImpressionRed'
+import BrightnessRed from '../red/brightnessRed'
+import ClarityRed from '../red/clarityRed'
+import IntensityRed from '../red/intensityRed'
+import AppearanceColorWhite from '../white/appearanceColorWhite'
+import AppearanceImpressionWhite from '../white/appearanceImpressionWhite'
+import BrightnessWhite from '../white/brightnessWhite'
+import IntensityWhite from '../white/intensityWhite'
+import Consistency from './consistency'
+
+type AppearanceAllSelection =
+  | ClarityRed
+  | BrightnessWhite
+  | BrightnessRed
+  | AppearanceColorWhite
+  | AppearanceColorRed
+  | IntensityWhite
+  | IntensityRed
+  | Consistency
+  | AppearanceImpressionWhite
+  | AppearanceImpressionRed
+
+export default AppearanceAllSelection
