4a5b2b39991463673b64718cdd6521f612f4d23d
yuma-matsui <oshino.iiyo@gmail.com>
2022-12-25 09:50:04 +0900

    濃淡の配列の重複をなくした

--- src/assets/collections/appearance/shared/intensityItems.ts
+++ src/assets/collections/appearance/shared/intensityItems.ts
@@ -1,5 +1,5 @@
 import { Intensity } from '../../../../types'
 
-const INTENSITY_ITEMS: Intensity[] = ['無色に近い', 'やや濃い', '濃い', '濃い']
+const INTENSITY_ITEMS: Intensity[] = ['無色に近い', 'やや濃い', '濃い', '非常に濃い']
 
 export default INTENSITY_ITEMS
