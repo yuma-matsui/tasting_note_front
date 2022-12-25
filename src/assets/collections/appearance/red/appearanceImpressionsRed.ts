4f4ca715a76517ae18bfba81292a0f9409dfc1cc
yuma-matsui <oshino.iiyo@gmail.com>
2022-12-25 16:01:26 +0900

    色調、外観の印象のコレクションから不要な/を削除した

--- src/assets/collections/appearance/red/appearanceImpressionsRed.ts
+++ src/assets/collections/appearance/red/appearanceImpressionsRed.ts
@@ -6,7 +6,7 @@ const APPEARANCE_IMPRESSIONS_RED: AppearanceImpressionRed[] = [
   'やや熟成した',
   '熟成した',
   '酸化熟成のニュアンス',
-  '酸化が進んだ /',
+  '酸化が進んだ',
   '軽快な',
   '成熟度が高い',
   '濃縮感が強い'
--- src/assets/collections/appearance/white/appearanceImpressionsWhite.ts
+++ src/assets/collections/appearance/white/appearanceImpressionsWhite.ts
@@ -5,7 +5,7 @@ const APPEARANCE_IMPRESSIONS_WHITE: AppearanceImpressionWhite[] = [
   'やや発展した',
   '熟成のニュアンスが見える',
   '熟成した',
-  '酸化が進んだ /',
+  '酸化が進んだ',
   '軽快な',
   '成熟度が高い',
   '濃縮感がある'
