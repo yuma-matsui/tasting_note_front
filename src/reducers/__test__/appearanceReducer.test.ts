14f55a885b3f6cd4d8ba19e70ce2521663a1f5e2
yuma-matsui <oshino.iiyo@gmail.com>
2022-12-27 22:47:45 +0900

    reducerのテストの繰り返し部分をeachでまとめた

--- src/reducers/__test__/appearanceReducer.test.ts
+++ src/reducers/__test__/appearanceReducer.test.ts
@@ -1,84 +1,52 @@
-import {
-  Appearance,
-  AppearanceColorWhite,
-  AppearanceImpressionWhite,
-  Brightness,
-  Clarity,
-  Consistency,
-  Intensity
-} from '../../types'
+import { Appearance, AppearanceAllLabels } from '../../types'
 import AppearanceName from '../../types/appearance/shared/appearanceName'
 import appearanceReducer from '../appearanceReducer'
 
 describe('appearanceReducer', () => {
-  let initialAppearance: Appearance = {
-    clarity: 'やや濁った',
-    brightness: 'モヤがかった',
+  let type: AppearanceName
+  const initialAppearance: Appearance = {
+    clarity: '',
+    brightness: '',
     appearanceColor: [],
-    intensity: 'やや濃い',
-    consistency: '強い',
+    intensity: '',
+    consistency: '',
     appearanceImpression: []
   }
 
-  describe('typeがclarityの場合', () => {
-    it('clarityプロパティの値がvalueの値に変更される', () => {
-      const type: AppearanceName = 'clarity'
-      const value: Clarity = '澄んだ'
-      const action = { type, payload: { value } }
+  describe('clarity, brightness, intensity, consistency', () => {
+    const testItems: [AppearanceName, AppearanceAllLabels][] = [
+      ['clarity', '澄んだ'],
+      ['brightness', '輝きのある'],
+      ['intensity', '濃い'],
+      ['consistency', 'やや強い']
+    ]
+    it.each(testItems)('typeが%sの場合プロパティの値が「%s」に変更される', (actionType, newValue) => {
+      const action = { type: actionType, payload: { value: newValue } }
       const newAppearance = appearanceReducer(initialAppearance, action)
-      expect(newAppearance.clarity).toBe(value)
+      expect(newAppearance[actionType]).toBe(newValue)
     })
   })
-  describe('typeがbrightnessの場合', () => {
-    it('brightnessプロパティの値がvalueの値に変更される', () => {
-      const type: AppearanceName = 'brightness'
-      const value: Brightness = '輝きのある'
-      const action = { type, payload: { value } }
-      const newAppearance = appearanceReducer(initialAppearance, action)
-      expect(newAppearance.brightness).toBe(value)
-    })
-  })
-  describe('typeがintensityの場合', () => {
-    it('intensityプロパティの値がvalueの値に変更される', () => {
-      const type: AppearanceName = 'intensity'
-      const value: Intensity = '濃い'
-      const action = { type, payload: { value } }
-      const newAppearance = appearanceReducer(initialAppearance, action)
-      expect(newAppearance.intensity).toBe(value)
-    })
-  })
-  describe('typeがconsistencyの場合', () => {
-    it('consistencyプロパティの値がvalueの値に変更される', () => {
-      const type: AppearanceName = 'consistency'
-      const value: Consistency = 'やや強い'
-      const action = { type, payload: { value } }
-      const newAppearance = appearanceReducer(initialAppearance, action)
-      expect(newAppearance.consistency).toBe(value)
-    })
-  })
+
   describe('typeがappearanceColorの場合', () => {
-    const type: AppearanceName = 'appearanceColor'
+    beforeEach(() => {
+      type = 'appearanceColor'
+      initialAppearance.appearanceColor = []
+    })
+
+    const firstColor: AppearanceAllLabels = 'アンバー'
+    const secondColor: AppearanceAllLabels = 'グリーンがかった'
+    const thirdColor: AppearanceAllLabels = 'トパーズ'
 
     describe('appearanceColorの初期値が空の配列の場合', () => {
       it('appearanceColorプロパティの配列にvalueが追加される', () => {
-        const value: AppearanceColorWhite = 'グリーンがかった'
-        const action = { type, payload: { value } }
+        const action = { type, payload: { value: firstColor } }
         const newAppearance = appearanceReducer(initialAppearance, action)
-        expect(newAppearance.appearanceColor).toEqual([value])
+        expect(newAppearance.appearanceColor).toEqual([firstColor])
       })
     })
     describe('appearanceColorの初期値が要素を1つ持つ配列の場合', () => {
       it('appearanceColorプロパティの配列にvalueが追加される', () => {
-        const firstColor: AppearanceColorWhite = 'アンバー'
-        const secondColor: AppearanceColorWhite = 'グリーンがかった'
-        initialAppearance = {
-          clarity: 'やや濁った',
-          brightness: 'モヤがかった',
-          appearanceColor: [firstColor],
-          intensity: 'やや濃い',
-          consistency: '強い',
-          appearanceImpression: []
-        }
+        initialAppearance.appearanceColor.push(firstColor)
         const action = { type, payload: { value: secondColor } }
         const newAppearance = appearanceReducer(initialAppearance, action)
         expect(newAppearance.appearanceColor).toEqual([firstColor, secondColor])
@@ -86,17 +54,7 @@ describe('appearanceReducer', () => {
     })
     describe('appearanceColorの初期値が要素を2つ以上持つ配列の場合', () => {
       it('appearanceColorプロパティの配列にvalueが追加されて先頭の要素が削除される', () => {
-        const firstColor: AppearanceColorWhite = 'アンバー'
-        const secondColor: AppearanceColorWhite = 'グリーンがかった'
-        const thirdColor: AppearanceColorWhite = 'トパーズ'
-        initialAppearance = {
-          clarity: 'やや濁った',
-          brightness: 'モヤがかった',
-          appearanceColor: [firstColor, secondColor],
-          intensity: 'やや濃い',
-          consistency: '強い',
-          appearanceImpression: []
-        }
+        initialAppearance.appearanceColor.push(firstColor, secondColor)
         const action = { type, payload: { value: thirdColor } }
         const newAppearance = appearanceReducer(initialAppearance, action)
         expect(newAppearance.appearanceColor).toEqual([secondColor, thirdColor])
@@ -104,28 +62,25 @@ describe('appearanceReducer', () => {
     })
   })
   describe('typeがappearanceImpressionの場合', () => {
-    const type: AppearanceName = 'appearanceImpression'
+    beforeEach(() => {
+      type = 'appearanceImpression'
+      initialAppearance.appearanceColor = []
+    })
+
+    const firstImpression: AppearanceAllLabels = '若々しい'
+    const secondImpression: AppearanceAllLabels = '軽快な'
+    const thirdImpression: AppearanceAllLabels = '酸化が進んだ'
 
     describe('appearanceImpressionの初期値が空の配列の場合', () => {
       it('appearanceImpressionプロパティの配列にvalueが追加される', () => {
-        const value: AppearanceImpressionWhite = '若々しい'
-        const action = { type, payload: { value } }
+        const action = { type, payload: { value: firstImpression } }
         const newAppearance = appearanceReducer(initialAppearance, action)
-        expect(newAppearance.appearanceImpression).toEqual([value])
+        expect(newAppearance.appearanceImpression).toEqual([firstImpression])
       })
     })
     describe('appearanceImpressionの初期値が要素を1つ持つ配列の場合', () => {
       it('appearanceImpressionプロパティの配列にvalueが追加される', () => {
-        const firstImpression: AppearanceImpressionWhite = '若々しい'
-        const secondImpression: AppearanceImpressionWhite = '軽快な'
-        initialAppearance = {
-          clarity: 'やや濁った',
-          brightness: 'モヤがかった',
-          appearanceColor: [],
-          intensity: 'やや濃い',
-          consistency: '強い',
-          appearanceImpression: [firstImpression]
-        }
+        initialAppearance.appearanceImpression.push(firstImpression)
         const action = { type, payload: { value: secondImpression } }
         const newAppearance = appearanceReducer(initialAppearance, action)
         expect(newAppearance.appearanceImpression).toEqual([firstImpression, secondImpression])
@@ -133,17 +88,7 @@ describe('appearanceReducer', () => {
     })
     describe('appearanceImpressionの初期値が要素を2つ以上持つ配列の場合', () => {
       it('appearanceImpressionプロパティの配列にvalueが追加されて先頭の要素が削除される', () => {
-        const firstImpression: AppearanceImpressionWhite = '若々しい'
-        const secondImpression: AppearanceImpressionWhite = '軽快な'
-        const thirdImpression: AppearanceImpressionWhite = '酸化が進んだ'
-        initialAppearance = {
-          clarity: 'やや濁った',
-          brightness: 'モヤがかった',
-          appearanceColor: [],
-          intensity: 'やや濃い',
-          consistency: '強い',
-          appearanceImpression: [firstImpression, secondImpression]
-        }
+        initialAppearance.appearanceImpression.push(firstImpression, secondImpression)
         const action = { type, payload: { value: thirdImpression } }
         const newAppearance = appearanceReducer(initialAppearance, action)
         expect(newAppearance.appearanceImpression).toEqual([secondImpression, thirdImpression])
