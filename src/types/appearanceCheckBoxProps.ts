72275138080dadcfa993fddc1aeef25dc360caa9
yuma-matsui <oshino.iiyo@gmail.com>
2022-12-29 10:38:57 +0900

    コンポーネントのprops用の型を格納するディレクトリを作った

--- src/types/appearanceCheckBoxProps.ts
+++
@@ -1,9 +0,0 @@
-import AppearanceAllLabels from './appearance/shared/appearanceAllLabels'
-import AppearanceName from './appearance/shared/appearanceName'
-
-type AppearanceCheckBoxProps = {
-  name: AppearanceName
-  label: AppearanceAllLabels
-}
-
-export default AppearanceCheckBoxProps
---
+++ src/types/props/appearanceCheckBoxProps.ts
@@ -0,0 +1,9 @@
+import AppearanceAllLabels from '../appearance/shared/appearanceAllLabels'
+import AppearanceName from '../appearance/shared/appearanceName'
+
+type AppearanceCheckBoxProps = {
+  name: AppearanceName
+  label: AppearanceAllLabels
+}
+
+export default AppearanceCheckBoxProps
