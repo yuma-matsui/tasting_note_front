d6e4f93d0af144242dc5951e598007806ac58125
yuma-matsui <oshino.iiyo@gmail.com>
2022-12-29 20:13:42 +0900

    果実のコレクションを用意した

---
+++ src/assets/collections/flavor/white/flavorFruitLabelsWhite.ts
@@ -0,0 +1,19 @@
+import { FlavorFruitTupleWhite } from '../../../../types/tasting_sheet/flavor'
+
+const FLAVOR_FRUIT_LABELS_WHITE: FlavorFruitTupleWhite = [
+  '柑橘類',
+  '青リンゴ',
+  'リンゴ',
+  '洋梨',
+  'マスカット',
+  '花梨',
+  'パッションフルーツ',
+  '白桃',
+  'アプリコット',
+  'パイナップル',
+  'ライチ',
+  'バナナ',
+  'マンゴー'
+]
+
+export default FLAVOR_FRUIT_LABELS_WHITE
---
+++ src/types/tasting_sheet/flavor/red/flavorFruitTupleRed.ts
@@ -0,0 +1,12 @@
+type FlavorFruitTupleRed = [
+  'イチゴ',
+  'ラズベリー',
+  'ブルーベリー',
+  'カシス',
+  'ブラックベリー',
+  'ブラックチェリー',
+  '干しプラム',
+  '乾燥イチジク'
+]
+
+export default FlavorFruitTupleRed
