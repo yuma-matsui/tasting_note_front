df7cd023fbc6e46baafe45aa3f5478e044dcfd85
yuma-matsui <oshino.iiyo@gmail.com>
2022-12-29 18:48:20 +0900

    Appearanceフォームをメモ化した

--- src/components/molecules/NewTastingSheetAppearanceForm.tsx
+++ src/components/molecules/NewTastingSheetAppearanceForm.tsx
@@ -1,11 +1,11 @@
-import { memo } from 'react'
+import { FC, memo } from 'react'
 
+import BaseForm from './shared/BaseForm'
 import { APPEARANCE_ITEMS_RED, APPEARANCE_ITEMS_WHITE } from '../../assets'
 import { useTastingSheetContext } from '../../hooks'
 import { TastingSheetAppearanceCheckBox } from '../atoms'
-import BaseForm from './shared/BaseForm'
 
-const NewTastingSheetAppearanceForm = memo(() => {
+const NewTastingSheetAppearanceForm: FC = memo(() => {
   const { tastingSheet } = useTastingSheetContext()
   const items = tastingSheet.color === 'white' ? APPEARANCE_ITEMS_WHITE : APPEARANCE_ITEMS_RED
 
