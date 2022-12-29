6978410bc04d4b81c0f72559fd295d4f930c768d
yuma-matsui <oshino.iiyo@gmail.com>
2022-12-29 13:59:21 +0900

    ラジオボタンコンポーネントの不要なプロップスを削除した

--- src/hooks/tasting_sheet/useWineColorStatus.ts
+++ src/hooks/tasting_sheet/useWineColorStatus.ts
@@ -1,8 +1,7 @@
-import { WINE_COLORS } from '../../assets'
 import { WineColor } from '../../types'
 
 const useWineColorStatus = () => {
-  const isWineColor = (color: string): color is WineColor => WINE_COLORS.includes(color)
+  const isWineColor = (color: string): color is WineColor => color === 'white' || color === 'red'
   return { isWineColor }
 }
 
