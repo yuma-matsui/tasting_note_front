f6832216db494ba0341738c117dc76033d2ad09a
yuma-matsui <oshino.iiyo@gmail.com>
2022-12-29 20:24:09 +0900

    香りの第一印象のコレクションを用意した

---
+++ src/assets/collections/flavor/red/flavorFirstImpressionLabelsRed.ts
@@ -0,0 +1,15 @@
+import { FlavorFirstImpressionTupleRed } from '../../../../types/tasting_sheet/flavor'
+
+const FLAVOR_FIRST_IMPRESSION_LABELS_RED: FlavorFirstImpressionTupleRed = [
+  '閉じている',
+  '控えめ',
+  '開いている',
+  '強い',
+  'チャーミングな',
+  '華やかな',
+  '濃縮感がある',
+  '深みのある',
+  '複雑な'
+]
+
+export default FLAVOR_FIRST_IMPRESSION_LABELS_RED
---
+++ src/assets/collections/flavor/white/flavorFirstImpressionLabelsWhite.ts
@@ -0,0 +1,16 @@
+import { FlavorFirstImpressionTupleWhite } from '../../../../types/tasting_sheet/flavor'
+
+const FLAVOR_FIRST_IMPRESSION_LABELS_WHITE: FlavorFirstImpressionTupleWhite = [
+  '閉じている',
+  '控えめ',
+  '開いている',
+  '力強い',
+  'フレッシュな',
+  'チャーミングな',
+  '華やかな',
+  '濃縮感がある',
+  '深みのある',
+  '複雑な'
+]
+
+export default FLAVOR_FIRST_IMPRESSION_LABELS_WHITE
