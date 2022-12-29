be83e4f79394667e86fa5008d21e99f7a1a47d45
yuma-matsui <oshino.iiyo@gmail.com>
2022-12-29 10:13:14 +0900

    Providerの呼び出し位置を変更した

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import { FC } from 'react'
-import { TastingSheetProvider } from './providers'
 import RouterConfig from './router/RouterConfig'
 
-const App: FC = () => (
-  <TastingSheetProvider>
-    <RouterConfig />
-  </TastingSheetProvider>
-)
+const App: FC = () => <RouterConfig />
 
 export default App
