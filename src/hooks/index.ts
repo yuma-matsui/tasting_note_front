01da8d9dd08d4cf3e61a2c986ace26d65c6ef014
yuma-matsui <oshino.iiyo@gmail.com>
2022-12-30 09:26:13 +0900

    FlavorContextをhooksに切り出した

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,3 +3,5 @@ export { default as useWineColorStatus } from './tasting_sheet/useWineColorStatu
 
 export { default as useAppearanceContext } from './tasting_sheet/appearance/useAppearanceContext'
 export { default as useAppearanceStatus } from './tasting_sheet/appearance/useAppearanceStatus'
+
+export { default as useFlavorContext } from './tasting_sheet/flavor/useFlavorContext'
---
+++ src/hooks/tasting_sheet/flavor/useFlavorContext.ts
@@ -0,0 +1,7 @@
+import { useContext } from 'react'
+
+import { FlavorContext } from '../../../contexts'
+
+const useFlavorContext = () => useContext(FlavorContext)
+
+export default useFlavorContext
