38e40df962cb96fda08301465c96cab579c110c6
yuma-matsui <oshino.iiyo@gmail.com>
2023-01-01 14:29:11 +0900

    味わいのフォームで使用するラベルのコレクションを用意した

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -7,3 +7,6 @@ export { default as APPEARANCE_ITEMS_WHITE } from './collections/appearance/whit
 
 export { default as FLAVOR_ITEMS_RED } from './collections/flavor/red/flavorItemsRed'
 export { default as FLAVOR_ITEMS_WHITE } from './collections/flavor/white/flavorItemsWhite'
+
+export { default as TASTE_ITEMS_RED } from './collections/taste/red/tasteItemsRed'
+export { default as TASTE_ITEMS_WHITE } from './collections/taste/white/tasteItemsWhite'
