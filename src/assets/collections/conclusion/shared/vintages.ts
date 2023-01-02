be08e606c798b5dbea956faf584905168c6c3621
yuma-matsui <oshino.iiyo@gmail.com>
2023-01-02 11:48:20 +0900

    ヴィンテージの配列を降順に変更した

--- src/assets/collections/conclusion/shared/vintages.ts
+++ src/assets/collections/conclusion/shared/vintages.ts
@@ -1,7 +1,6 @@
-const START_YEAR = 1900
+const END_YEAR = 1900
+const thisYear = new Date().getFullYear()
 
-const VINTAGES = [
-  ...new Array(new Date().getFullYear() - START_YEAR).fill(START_YEAR).map((year: number, index) => year + index)
-]
+const VINTAGES = [...new Array(thisYear - END_YEAR).fill(thisYear).map((year: number, index) => year - index - 1)]
 
 export default VINTAGES
