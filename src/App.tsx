38d62bcece5728133f2a36237065424c387beebe
yuma-matsui <oshino.iiyo@gmail.com>
2023-01-02 16:19:47 +0900

    chakrauiをインストールした

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { FC } from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+
 import RouterConfig from './router/RouterConfig'
 
-const App: FC = () => <RouterConfig />
+const App: FC = () => (
+  <ChakraProvider>
+    <RouterConfig />
+  </ChakraProvider>
+)
 
 export default App
