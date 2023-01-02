a5b2deb0260caca514ff0380144fd0868d752837
yuma-matsui <oshino.iiyo@gmail.com>
2023-01-02 11:34:53 +0900

    まとめフォームのセレクトボックス表示コンポーネントを作った

---
+++ src/components/molecules/ConclusionSelectBoxes.tsx
@@ -0,0 +1,20 @@
+import { CONCLUSION_SELECT_OPTIONS_RED, CONCLUSION_SELECT_OPTIONS_WHITE } from '../../assets'
+import { useTastingSheetContext } from '../../hooks'
+import { PolymorphicSelectBox } from '../atoms'
+
+const ConclusionSelectBoxes = () => {
+  const {
+    tastingSheet: { color }
+  } = useTastingSheetContext()
+  const items = color === 'white' ? CONCLUSION_SELECT_OPTIONS_WHITE : CONCLUSION_SELECT_OPTIONS_RED
+
+  return (
+    <>
+      {items.map(({ label, name, options }) => (
+        <PolymorphicSelectBox key={label} label={label} name={name} options={options.map((option) => String(option))} />
+      ))}
+    </>
+  )
+}
+
+export default ConclusionSelectBoxes
