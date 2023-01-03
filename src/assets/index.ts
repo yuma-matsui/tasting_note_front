df6c6b712ae29b223c4c9fc5c6e88cb1e61ce439
yuma-matsui <oshino.iiyo@gmail.com>
2023-01-03 16:15:20 +0900

    不要なコレクションのファイルを削除した

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,18 +1,13 @@
 export { default as WINE_COLORS } from './collections/wineColors'
 export { default as TASTING_TIME } from './collections/tastingTime'
-export { default as FORM_ITEMS } from './collections/formItems'
 
-export { default as APPEARANCE_ITEMS_RED } from './collections/appearance/red/appearanceItemsRed'
-export { default as APPEARANCE_ITEMS_WHITE } from './collections/appearance/white/appearanceItemsWhite'
+export { default as APPEARANCE_FORM_ITEMS } from './collections/appearance/shared/appearanceFormItems'
 
-export { default as FLAVOR_ITEMS_RED } from './collections/flavor/red/flavorItemsRed'
-export { default as FLAVOR_ITEMS_WHITE } from './collections/flavor/white/flavorItemsWhite'
+export { default as FLAVOR_FORM_ITEMS } from './collections/flavor/shared/flavorFormItems'
 
-export { default as TASTE_ITEMS_RED } from './collections/taste/red/tasteItemsRed'
-export { default as TASTE_ITEMS_WHITE } from './collections/taste/white/tasteItemsWhite'
+export { default as TASTE_FORM_ITEMS } from './collections/taste/shared/tasteFormItems'
 
-export { default as CONCLUSION_ITEMS_RED } from './collections/conclusion/red/conclusionItemsRed'
-export { default as CONCLUSION_ITEMS_WHITE } from './collections/conclusion/white/conclusionItemsWhite'
+export { default as CONCLUSION_FORM_ITEMS } from './collections/conclusion/shared/conclusionFormItems'
 export { default as CONCLUSION_SELECT_OPTIONS_RED } from './collections/conclusion/red/conclusionSelectOptionsRed'
 export { default as CONCLUSION_SELECT_OPTIONS_WHITE } from './collections/conclusion/white/conclusionSelectOptionsWhite'
 export { default as VINTAGES } from './collections/conclusion/shared/vintages'
