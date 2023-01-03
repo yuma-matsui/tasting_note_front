53acc35b4ffb82bcf04f5fbda84e9b8f6b012ec3
yuma-matsui <oshino.iiyo@gmail.com>
2023-01-03 19:28:24 +0900

    テイスティングシートの数値型のプロパティの初期値にnullを許容した

--- src/assets/collections/conclusion/shared/vintages.ts
+++ src/assets/collections/conclusion/shared/vintages.ts
@@ -1,6 +1,8 @@
 const END_YEAR = 1900
 const thisYear = new Date().getFullYear()
 
-const VINTAGES = [...new Array(thisYear - END_YEAR).fill(thisYear).map((year: number, index) => year - index - 1)]
+const VINTAGES = [
+  ...new Array(thisYear - END_YEAR).fill(thisYear).map((year: number, index) => String(year - index - 1))
+]
 
 export default VINTAGES
--- src/assets/collections/tastingTime.ts
+++ src/assets/collections/tastingTime.ts
@@ -1,3 +1,3 @@
-const TASTING_TIME = [3, 5, 10, 15]
+const TASTING_TIME = ['3', '5', '10', '15']
 
 export default TASTING_TIME
