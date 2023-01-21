e045be925473e287ee821e917bc27d7418f4ff0e
yuma-matsui <oshino.iiyo@gmail.com>
2023-01-21 19:25:04 +0900

    回答途中で離脱した後に再訪したときにステートがリフレッシュされるようにした

--- src/components/atoms/buttons/StartTastingButton.tsx
+++ src/components/atoms/buttons/StartTastingButton.tsx
@@ -1,8 +1,14 @@
 import { useNavigate } from 'react-router-dom'
+import { useTastingSheetContext } from '../../../hooks'
+import { initialTastingSheet } from '../../../utils'
 
 const StartTastingButton = () => {
+  const { setTastingSheet } = useTastingSheetContext()
   const navigate = useNavigate()
-  const onClick = () => navigate('/tasting_sheets/new')
+  const onClick = () => {
+    navigate('/tasting_sheets/new')
+    setTastingSheet({ ...initialTastingSheet })
+  }
 
   return (
     <button type="button" onClick={onClick}>
