017e7bd5092f3f27874714cfc83afa4f6e2fce8e
yuma-matsui <oshino.iiyo@gmail.com>
2023-01-22 16:59:21 +0900

    条件によってフッターとヘッダーの表示を切り替えられるようにした

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,21 +1,17 @@
 import { FC, memo } from 'react'
+
+import { HeaderFooterProps } from '../../types'
 import { FooterLink } from '../atoms'
 
-const Footer: FC = memo(() => (
-  <footer
-    style={{
-      borderTop: '1px solid gray',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      marginTop: '16px'
-    }}
-  >
-    <FooterLink />
-    <ul style={{ display: 'flex', listStyle: 'none' }}>
-      <li style={{ marginRight: '16px' }}>利用規約</li>
-      <li>プライバシーポリシー</li>
-    </ul>
+const Footer: FC<HeaderFooterProps> = memo(({ logoOnly = false, modalId }) => (
+  <footer className="flex flex-col items-center border-t border-gray-500 pt-4 mt-4">
+    <FooterLink hasModal={logoOnly} modalId={modalId} />
+    {!logoOnly && (
+      <ul className="flex list-none">
+        <li className="mr-4">利用規約</li>
+        <li>プライバシーポリシー</li>
+      </ul>
+    )}
     <p>&copy; 2022 yuma-matsui</p>
   </footer>
 ))
