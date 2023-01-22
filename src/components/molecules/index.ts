db643c870ea3e78dca078b26169a86eb9cd81fea
yuma-matsui <oshino.iiyo@gmail.com>
2023-01-22 16:58:25 +0900

    ヘッダーとフッターのロゴ部分をコンポーネントに切り出した

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -2,4 +2,9 @@ export { default as NewTastingSheetSettingForm } from './forms/NewTastingSheetSe
 export { default as TastingSheetBaseForm } from './forms/TastingSheetBaseForm'
 
 export { default as TastingSheetConfirmationDataList } from './TastingSheetConfirmationDataList'
-export { default as TastingSheetFormModalBox } from './TastingSheetFormModalBox'
+
+export { default as TastingSheetFormModalBox } from './modals/TastingSheetFormModalBox'
+export { default as EditCheckModalBox } from './modals/EditCheckModalBox'
+
+export { default as FooterLogo } from './logos/FooterLogo'
+export { default as HeaderLogo } from './logos/HeaderLogo'
