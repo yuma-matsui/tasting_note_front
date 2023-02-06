6491608acd6b856907396785f23b452c379c1726
yuma-matsui <oshino.iiyo@gmail.com>
2023-02-06 21:50:47 +0900

    unsafeなasを削除して初期値をセットした

--- src/contexts/TastingSheetContext.ts
+++ src/contexts/TastingSheetContext.ts
@@ -1,6 +1,10 @@
 import { createContext } from 'react'
 import { TastingSheetContextType } from '../types'
+import { initialTastingSheet } from '../utils'
 
-const TastingSheetContext = createContext<TastingSheetContextType>({} as TastingSheetContextType)
+const TastingSheetContext = createContext<TastingSheetContextType>({
+  tastingSheet: initialTastingSheet,
+  setTastingSheet: () => {}
+})
 
 export default TastingSheetContext
