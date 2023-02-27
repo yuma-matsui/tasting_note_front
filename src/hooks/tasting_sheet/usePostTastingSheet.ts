a204fb89330b27f3119f6a93d57b748f3affee7e
yuma-matsui <oshino.iiyo@gmail.com>
2023-02-27 22:58:41 +0900

    テイスティングシートの新規作成処理を実装した

--- src/hooks/tasting_sheet/usePostTastingSheet.ts
+++ src/hooks/tasting_sheet/usePostTastingSheet.ts
@@ -1,25 +1,44 @@
 import { User } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
 
+import { TastingSheetApi } from '../../types'
 import useAuthContext from '../useAuthContext'
+import useAxios from '../useAxios'
 import useTastingSheetContext from './useTastingSheetContext'
 
 const usePostTastingSheet = () => {
   const navigate = useNavigate()
-  const { tastingSheet } = useTastingSheetContext()
+  const { tastingSheet, setPosting } = useTastingSheetContext()
   const { signIn, currentUser } = useAuthContext()
+  const { client, getHeaders } = useAxios()
 
-  const postTastingSheet = (user?: User) => {
+  const postTastingSheet = async (user?: User) => {
+    setPosting(true)
     let postingUser: User | undefined | null = currentUser
     if (user) postingUser = user
 
     if (!postingUser) throw new Error('不正な呼び出し方です。')
 
-    console.log('APIリクエスト', postingUser, tastingSheet)
-    navigate('/')
+    try {
+      const response = (
+        await client.post<TastingSheetApi>('/tasting_sheets', tastingSheet, await getHeaders(postingUser))
+      ).data
+      console.log(response)
+      navigate('/')
+    } catch (e) {
+      if (e instanceof Error) throw e
+    } finally {
+      setPosting(false)
+    }
   }
 
-  const signInAndPostTastingSheet = async () => postTastingSheet((await signIn())?.user)
+  const signInAndPostTastingSheet = async () => {
+    try {
+      await postTastingSheet((await signIn())?.user)
+    } catch (e) {
+      if (e instanceof Error) throw e
+    }
+  }
 
   return {
     postTastingSheet,
