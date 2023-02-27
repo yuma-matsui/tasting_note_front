b01185bf152d2e81c6999968338e44a0dffc90a2
yuma-matsui <oshino.iiyo@gmail.com>
2023-02-27 22:54:00 +0900

    条件分岐の条件式をシンプルにした

--- src/components/templates/FormController.tsx
+++ src/components/templates/FormController.tsx
@@ -34,12 +34,12 @@ const FormController: FC<FormControllerProps> = memo(
           <FormControllerButton value={nextButtonText} disabled={disabled} onClick={() => onClick('next', submitRef)} />
         )}
         {isLastStep &&
-          (currentUser === null ? (
-            <ModalOpenButton id={lastStepModalId} text="提出する" />
-          ) : (
+          (currentUser ? (
             <button type="button" onClick={onClickPost} className="btn">
               提出する
             </button>
+          ) : (
+            <ModalOpenButton id={lastStepModalId} text="提出する" />
           ))}
         <input type="submit" hidden ref={submitRef} />
         <TastingSheetFormModalBox id={lastStepModalId} />
