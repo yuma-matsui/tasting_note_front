e1fe03c80a760f25b3844b5279c0ea1444f4af52
yuma-matsui <oshino.iiyo@gmail.com>
2023-03-04 12:11:01 +0900

    テイスティングシートの個別情報を表示するカードコンポーネントを作った

---
+++ src/components/molecules/TastingSheetCard.tsx
@@ -0,0 +1,30 @@
+import { FC, memo } from 'react'
+
+import { TastingSheetCardProps } from '../../types'
+import defaultImage from '../../assets/images/wineGlass.jpg'
+import { DeleteTastingSheetButton } from '../atoms'
+
+const TastingSheetCard: FC<TastingSheetCardProps> = memo(({ tastingSheet }) => {
+  const bgColor = tastingSheet.color === 'red' ? 'bg-red-600' : 'bg-emerald-500'
+  const textColor = tastingSheet.color === 'red' ? 'text-red-700' : 'text-emerald-500'
+
+  return (
+    <li className="mb-4">
+      <div className={`card w-96 shadow-xl relative ${bgColor}`}>
+        <figure>
+          <img src={defaultImage} alt="wine" />
+        </figure>
+        <p className={`absolute top-2 left-2 ${textColor}`}>No Image</p>
+        <div className="card-body text-white">
+          <h2 className="card-title">{tastingSheet.name}</h2>
+          <p>{tastingSheet.createdAt}</p>
+          <div className="card-actions justify-end">
+            <DeleteTastingSheetButton id={tastingSheet.id} />
+          </div>
+        </div>
+      </div>
+    </li>
+  )
+})
+
+export default TastingSheetCard
--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,12 +1,12 @@
 export { default as NewTastingSheetSettingForm } from './forms/NewTastingSheetSettingForm'
 export { default as TastingSheetBaseForm } from './forms/TastingSheetBaseForm'
 
-export { default as TastingSheetConfirmationDataList } from './TastingSheetConfirmationDataList'
-
 export { default as TastingSheetFormModalBox } from './modals/TastingSheetFormModalBox'
 export { default as EditCheckModalBox } from './modals/EditCheckModalBox'
 
 export { default as FooterLogo } from './logos/FooterLogo'
 export { default as HeaderLogo } from './logos/HeaderLogo'
 
+export { default as TastingSheetConfirmationDataList } from './TastingSheetConfirmationDataList'
 export { default as StepsBar } from './StepsBar'
+export { default as TastingSheetCard } from './TastingSheetCard'
