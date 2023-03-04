5760bbd6e2852f23f8c3701a54fa316578e55db2
yuma-matsui <oshino.iiyo@gmail.com>
2023-03-04 10:19:50 +0900

    テイスティングシートが存在しない場合に表示するコンポーネントを用意した

---
+++ src/components/organisms/SignedInTopPageInstruction.tsx
@@ -0,0 +1,15 @@
+import { FC, memo } from 'react'
+
+const SignedInTopPageInstruction: FC = memo(() => (
+  <div className="flex flex-col items-center">
+    <h1>テイスティングを記録しよう</h1>
+    <div className="w-72 h-72 bg-gray-500 text-slate-50">サービスのデモ画像配置予定</div>
+    <ul>
+      <li>本番同様のシートを使用してテスト形式でワインを評価</li>
+      <li>評価したコメントシートを記録</li>
+      <li>記録したシートで復習</li>
+    </ul>
+  </div>
+))
+
+export default SignedInTopPageInstruction
--- src/components/pages/SignedInWelcomePage.tsx
+++ src/components/pages/SignedInWelcomePage.tsx
@@ -1,21 +1,23 @@
 import { FC, memo } from 'react'
+import { useAllTastingSheets, useAuthContext } from '../../hooks'
 import { StartTastingButton } from '../atoms'
+import { SignedInTopPageInstruction } from '../organisms'
 
 import { DefaultLayout } from '../templates'
 
-const SignedInWelcomePage: FC = memo(() => (
-  <DefaultLayout>
-    <div className="flex flex-col items-center">
-      <h1>テイスティングを記録しよう</h1>
-      <div className="w-72 h-72 bg-gray-500 text-slate-50">サービスのデモ画像配置予定</div>
-      <ul>
-        <li>本番同様のシートを使用してテスト形式でワインを評価</li>
-        <li>評価したコメントシートを記録</li>
-        <li>記録したシートで復習</li>
-      </ul>
-      <StartTastingButton />
-    </div>
-  </DefaultLayout>
-))
+const SignedInWelcomePage: FC = memo(() => {
+  const { tastingSheets, fetching, hasTastingSheets } = useAllTastingSheets()
+
+  if (fetching) return <p>...Loading</p>
+
+  return (
+    <DefaultLayout>
+      <div className="flex flex-col items-center">
+        {hasTastingSheets ? <p>テイスティグシート一覧</p> : <SignedInTopPageInstruction />}
+        <StartTastingButton />
+      </div>
+    </DefaultLayout>
+  )
+})
 
 export default SignedInWelcomePage
