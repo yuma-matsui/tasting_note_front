cbc9e4c16490ab06a2df427cce2d286da9208255
yuma-matsui <oshino.iiyo@gmail.com>
2023-03-04 22:41:00 +0900

    サインインとテイスティングシートの新規作成を行うボタンをコンポーネント化した

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -5,7 +5,11 @@ export { default as StartTastingButton } from './buttons/StartTastingButton'
 export { default as ModalOpenButton } from './buttons/ModalOpenButton'
 export { default as FormControllerButton } from './buttons/FormControllerButton'
 export { default as DeleteTastingSheetButton } from './buttons/DeleteTastingSheetButton'
+export { default as GoToTopPageButton } from './buttons/GoToTopPageButton'
+export { default as SignInAndPostButton } from './buttons/SignInAndPostButton'
 
 export { default as TastingSheetCheckBox } from './inputs/TastingSheetCheckBox'
 export { default as TastingSheetSelectBox } from './selects/TastingSheetSelectBox'
 export { default as TastingSheetTimer } from './TastingSheetTimer'
+
+export { default as ModalCloseLabel } from './labels/ModalCloseLabel'
