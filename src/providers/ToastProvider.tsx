d7a79c468def1d68e419f9e1e220e0f6099b8f8e
yuma-matsui <oshino.iiyo@gmail.com>
2023-03-05 14:50:10 +0900

    Loadingが終了した後にトーストを表示するようにした

--- src/providers/ToastProvider.tsx
+++ src/providers/ToastProvider.tsx
@@ -1,23 +1,31 @@
-import { FC, useMemo, useState } from 'react'
+import { FC, useCallback, useMemo, useState } from 'react'
 import { createPortal } from 'react-dom'
 
 import { BaseToast } from '../components/atoms'
 import { ToastContext } from '../contexts'
+import { useAuthContext, useTastingSheetsContext } from '../hooks'
 import { ReactNodeChildren } from '../types'
 
 const DISPLAYING_MILLISECOND = 2000
 
 const ToastProvider: FC<ReactNodeChildren> = ({ children }) => {
+  const { loading } = useAuthContext()
+  const { requesting } = useTastingSheetsContext()
+
   const [visible, setVisible] = useState(false)
   const [toastText, setToastText] = useState<string>('')
 
-  const showToast = (text: string) => {
-    setToastText(text)
-    setVisible(true)
-    window.setTimeout(() => setVisible(false), DISPLAYING_MILLISECOND)
-  }
+  const showToast = useCallback(
+    (text: string) => {
+      if (loading || requesting) return
+      setToastText(text)
+      setVisible(true)
+      window.setTimeout(() => setVisible(false), DISPLAYING_MILLISECOND)
+    },
+    [loading, requesting]
+  )
 
-  const toastState = useMemo(() => ({ showToast }), [])
+  const toastState = useMemo(() => ({ showToast }), [showToast])
 
   return (
     <ToastContext.Provider value={toastState}>
