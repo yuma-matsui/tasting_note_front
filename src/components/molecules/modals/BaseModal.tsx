ecfded62a64b28644593e8c9649304e9659ff3c3
yuma-matsui <oshino.iiyo@gmail.com>
2023-03-05 17:59:31 +0900

    モーダルのベースコンポーネントを作った

---
+++ src/components/molecules/modals/BaseModal.tsx
@@ -0,0 +1,30 @@
+import { FC, memo } from 'react'
+import { useModalContext } from '../../../hooks'
+
+import { BaseModalProps } from '../../../types'
+
+const BaseModalBox: FC<BaseModalProps> = memo(({ text, content, visible, closeText }) => {
+  const { setVisible } = useModalContext()
+  const onClickClose = () => setVisible(false)
+
+  return (
+    <>
+      <input type="checkbox" className="modal-toggle" checked={visible} />
+      <div className="modal">
+        <div className="modal-box">
+          <p>{text}</p>
+          <div className="modal-action flex items-center">
+            {closeText && (
+              <button type="button" onClick={onClickClose}>
+                {closeText}
+              </button>
+            )}
+            {content}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+})
+
+export default BaseModalBox
