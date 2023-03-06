7dec71732c056b30b343bafd22ca4bc1eef311e9
yuma-matsui <oshino.iiyo@gmail.com>
2023-03-06 21:55:30 +0900

    アカウト削除リンクを押された時にモーダルを表示するようにした

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -6,6 +6,7 @@ export { default as FormControllerButton } from './buttons/FormControllerButton'
 export { default as DeleteTastingSheetButton } from './buttons/DeleteTastingSheetButton'
 export { default as GoToTopPageButton } from './buttons/GoToTopPageButton'
 export { default as SignInAndPostButton } from './buttons/SignInAndPostButton'
+export { default as DeleteAccountButton } from './buttons/DeleteAccountButton'
 
 export { default as TastingSheetCheckBox } from './inputs/TastingSheetCheckBox'
 export { default as TastingSheetSelectBox } from './selects/TastingSheetSelectBox'
