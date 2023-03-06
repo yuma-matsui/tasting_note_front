78451bdb5dfd0864ece9bc71905b7b8a416e75fe
yuma-matsui <oshino.iiyo@gmail.com>
2023-03-06 20:37:51 +0900

    不要なコンポーネントを削除した

--- src/components/atoms/labels/ModalCloseLabel.tsx
+++
@@ -1,6 +0,0 @@
-import { FC, memo } from 'react'
-import { ModalCloseLabelProps } from '../../../types'
-
-const ModalCloseLabel: FC<ModalCloseLabelProps> = memo(({ id, text }) => <label htmlFor={id}>{text}</label>)
-
-export default ModalCloseLabel
