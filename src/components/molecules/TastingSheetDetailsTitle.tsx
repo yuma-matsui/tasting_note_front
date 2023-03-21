11662c2e02fa7cfa30cd93c6f1688236257ba1cc
yuma-matsui <oshino.iiyo@gmail.com>
2023-03-21 18:20:21 +0900

    テイスティングシート個別ページで表示するタイトルをコンポーネントに切り出した

---
+++ src/components/molecules/TastingSheetDetailsTitle.tsx
@@ -0,0 +1,25 @@
+import { FC, memo, useState } from 'react'
+
+import { useTastingSheetContext } from '../../hooks'
+import UpdateSheetNameForm from './forms/UpdateSheetNameForm'
+
+const TastingSheetDetailsTitle: FC = memo(() => {
+  const { tastingSheet } = useTastingSheetContext()
+  const [isEditing, setIsEditing] = useState(false)
+  const onClick = () => setIsEditing(true)
+
+  return isEditing ? (
+    <UpdateSheetNameForm setIsEditing={setIsEditing} />
+  ) : (
+    <h2>
+      {tastingSheet.name}
+      <span>
+        <button type="button" className="btn" onClick={onClick}>
+          変更
+        </button>
+      </span>
+    </h2>
+  )
+})
+
+export default TastingSheetDetailsTitle
--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,7 +1,6 @@
 export { default as NewTastingSheetSettingForm } from './forms/NewTastingSheetSettingForm'
 export { default as TastingSheetBaseForm } from './forms/TastingSheetBaseForm'
 export { default as TastingSheetSearchForm } from './forms/TastingSheetSearchForm'
-export { default as UpdateSheetNameForm } from './forms/UpdateSheetNameForm'
 export { default as NewWineForm } from './forms/NewWineForm'
 
 export { default as BaseModal } from './modals/BaseModal'
