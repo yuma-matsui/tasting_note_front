4b5c2b6486ca7a9cb57e93ee4ce51c09d46c69b7
yuma-matsui <oshino.iiyo@gmail.com>
2023-03-21 17:13:55 +0900

    バックエンドから返却されるテイスティングシートの型にwineプロパティを追加して個別ページでワイン情報を表示できるようにした

--- src/types/api/tastingSheetApi.ts
+++ src/types/api/tastingSheetApi.ts
@@ -1,10 +1,10 @@
 import TastingSheet from '../tasting_sheet/tastingSheet'
-import Wine from '../wine/wine'
+import WineApi from './wineApi'
 
 type TastingSheetApi = TastingSheet & {
   id: number
   createdAt: string
-  wine: Wine | null
+  wine: WineApi | null
 }
 
 export default TastingSheetApi
