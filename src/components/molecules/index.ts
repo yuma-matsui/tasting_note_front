178e8f07845e358fe21ecd17103ea2fa36d12fbf
yuma-matsui <oshino.iiyo@gmail.com>
2023-03-25 09:08:53 +0900

    ワイン詳細情報のタイトルをコンポーネントに切り出した

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -11,3 +11,5 @@ export { default as HeaderLogo } from './logos/HeaderLogo'
 export { default as SheetOrWineDetailsDataList } from './SheetOrWineDetailsDataList'
 export { default as StepsBar } from './StepsBar'
 export { default as TastingSheetCard } from './TastingSheetCard'
+
+export { default as WineDetailsTitle } from './titles/WineDetailsTitle'
--- src/components/organisms/WineDetails.tsx
+++ src/components/organisms/WineDetails.tsx
@@ -1,30 +1,15 @@
 import { FC, memo } from 'react'
-import { useOnClickOpenModal } from '../../hooks'
 
 import { WineApi } from '../../types'
-import { DeleteWineButton } from '../atoms'
+import { WineDetailsTitle } from '../molecules'
 import WineDetailLists from './WineDetailLists'
 
-const WineDetails: FC<{ wine: WineApi }> = memo(({ wine }) => {
-  const { onClickOpenModal } = useOnClickOpenModal({
-    text: '登録したワインを削除しますがよろしいですか？',
-    rightButton: <DeleteWineButton wine={wine} />
-  })
-
-  return (
-    <>
-      <h3>
-        {wine.name}
-        <span className="text-red-400">
-          <button type="button" onClick={onClickOpenModal}>
-            削除
-          </button>
-        </span>
-      </h3>
-      <div className="w-72 h-72 bg-gray-500 text-slate-50">ワイン画像を配置予定</div>
-      <WineDetailLists wine={wine} />
-    </>
-  )
-})
+const WineDetails: FC<{ wine: WineApi }> = memo(({ wine }) => (
+  <>
+    <WineDetailsTitle wine={wine} />
+    <div className="w-72 h-72 bg-gray-500 text-slate-50">ワイン画像を配置予定</div>
+    <WineDetailLists wine={wine} />
+  </>
+))
 
 export default WineDetails
