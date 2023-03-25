0d5034cafd70e9fddb5ffdbda35b00c592bb6203
yuma-matsui <oshino.iiyo@gmail.com>
2023-03-25 14:28:58 +0900

    テイスティングシート情報を取得するhooks内からグローバルステートを取り除いた

--- src/components/pages/TastingSheetDetailsPage.tsx
+++ src/components/pages/TastingSheetDetailsPage.tsx
@@ -1,14 +1,19 @@
-import { FC } from 'react'
+import { FC, memo, useEffect } from 'react'
 
-import { useFetchATastingSheet, useTastingSheetLabels } from '../../hooks'
+import { useFetchATastingSheet, useTastingSheetContext, useTastingSheetLabels } from '../../hooks'
 import { GoToAnotherPageButton, GoToNewWinePageButton } from '../atoms'
 import TastingSheetDetailsTitle from '../molecules/titles/TastingSheetDetailsTitle'
 import { TastingSheetDetailsTab, WineDetails } from '../organisms'
 import { DefaultLayout } from '../templates'
 
-const TastingSheetDetailsPage: FC<{ tastingSheetId: number }> = ({ tastingSheetId }) => {
-  const { fetching, wine } = useFetchATastingSheet(tastingSheetId)
+const TastingSheetDetailsPage: FC<{ tastingSheetId: number }> = memo(({ tastingSheetId }) => {
+  const { setTastingSheet } = useTastingSheetContext()
+  const { fetching, tastingSheet } = useFetchATastingSheet(tastingSheetId)
   const labels = useTastingSheetLabels()
+
+  useEffect(() => {
+    setTastingSheet(tastingSheet)
+  }, [setTastingSheet, tastingSheet])
 
   if (fetching) return <p>...Loading</p>
 
@@ -16,17 +21,17 @@ const TastingSheetDetailsPage: FC<{ tastingSheetId: number }> = ({ tastingSheetI
     <DefaultLayout>
       <TastingSheetDetailsTitle />
       <TastingSheetDetailsTab labels={labels} />
-      {!wine && <GoToNewWinePageButton />}
-      {wine && (
+      {!tastingSheet.wine && <GoToNewWinePageButton />}
+      {tastingSheet.wine && (
         <>
           <div className="divider" />
-          <WineDetails wine={wine} />
+          <WineDetails wine={tastingSheet.wine} />
         </>
       )}
       <div className="divider" />
       <GoToAnotherPageButton text="戻る" to="/" />
     </DefaultLayout>
   )
-}
+})
 
 export default TastingSheetDetailsPage
--- src/hooks/api/useFetchATastingSheet.ts
+++ src/hooks/api/useFetchATastingSheet.ts
@@ -1,41 +1,41 @@
 import { useCallback, useLayoutEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-import { TastingSheetApi, WineApi } from '../../types'
+import { TastingSheetApi } from '../../types'
+import { initialTastingSheet } from '../../utils'
 import useAuthContext from '../context/useAuthContext'
-import useTastingSheetContext from '../context/useTastingSheetContext'
 import useAxios from '../useAxios'
 
 const useFetchATastingSheet = (tastingSheetId: number) => {
   const navigate = useNavigate()
 
   const { client, getHeaders } = useAxios()
-  const { setTastingSheet } = useTastingSheetContext()
   const { currentUser } = useAuthContext()
 
-  const [wine, setWine] = useState<WineApi | null>(null)
+  const [tastingSheet, setTastingSheet] = useState<TastingSheetApi>({
+    ...initialTastingSheet,
+    id: 0,
+    createdAt: '',
+    wine: null
+  })
   const [fetching, setFetching] = useState(false)
   const fetchATastingSheet = useCallback(async () => {
     setFetching(true)
-    if (!currentUser) return
+    if (!currentUser) throw new Error('不正な呼び出し方です。')
 
     try {
       const { data: tastingSheetApi } = await client.get<TastingSheetApi | null>(
         `/tasting_sheets/${tastingSheetId}`,
         await getHeaders(currentUser)
       )
-      if (!tastingSheetApi) {
-        navigate('/')
-      } else {
-        setTastingSheet(tastingSheetApi)
-        setWine(tastingSheetApi.wine)
-      }
+      if (!tastingSheetApi) navigate('/')
+      if (tastingSheetApi) setTastingSheet(tastingSheetApi)
     } catch (e) {
       if (e instanceof Error) throw e
     } finally {
       setFetching(false)
     }
-  }, [currentUser, client, tastingSheetId, getHeaders, navigate, setTastingSheet])
+  }, [currentUser, client, tastingSheetId, getHeaders, navigate])
 
   useLayoutEffect(() => {
     if (currentUser)
@@ -47,7 +47,7 @@ const useFetchATastingSheet = (tastingSheetId: number) => {
 
   return {
     fetching,
-    wine
+    tastingSheet
   }
 }
 
