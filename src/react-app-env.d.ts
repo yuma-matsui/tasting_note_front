08e409da504fecde37cb344cd89caccd5e84758f
yuma-matsui <oshino.iiyo@gmail.com>
2023-03-28 22:30:04 +0900

    画像取得用のURLのドメインを環境変数に仕込んだ

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -10,5 +10,6 @@ namespace NodeJS {
     readonly REACT_APP_FIREBASE_APP_ID: string
     readonly REACT_APP_FIREBASE_MEASUREMENT_ID: string
     readonly REACT_APP_API_URL: string
+    readonly REACT_APP_CF_DOMAIN: string
   }
 }
