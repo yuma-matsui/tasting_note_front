e2709a056ccf4aa2dcd30808df5eb2f4644b8e99
yuma-matsui <oshino.iiyo@gmail.com>
2023-04-01 13:13:03 +0900

    テイスティングシート名を変更するhooksの実装を変更してグローバルステートの使用を取り除いた

--- src/hooks/api/useUpdateTastingSheetName.ts
+++ src/hooks/api/useUpdateTastingSheetName.ts
@@ -1,9 +1,9 @@
 import { useParams } from 'react-router-dom'
 
-import { TastingSheet, TastingSheetApi } from '../../types'
+import { TastingSheet } from '../../types'
 import useAuthContext from '../context/useAuthContext'
-import useTastingSheetContext from '../context/useTastingSheetContext'
 import useTastingSheetsContext from '../context/useTastingSheetsContext'
+import useToastContext from '../context/useToastContext'
 import useAxios from '../useAxios'
 
 const useUpdateTastingSheetName = () => {
@@ -11,26 +11,22 @@ const useUpdateTastingSheetName = () => {
   const target = Number(tastingSheetId)
 
   const { currentUser } = useAuthContext()
-  const { setTastingSheet } = useTastingSheetContext()
   const { setRequesting } = useTastingSheetsContext()
   const { client, getHeaders } = useAxios()
+  const { showToast } = useToastContext()
 
   const updateSheetName = async (tastingSheet: TastingSheet) => {
     if (!currentUser || Number.isNaN(target)) throw new Error('不正な呼び出し方です。')
     setRequesting(true)
 
     try {
-      const { data: tastingSheetApi } = await client.put<TastingSheetApi>(
-        `/tasting_sheets/${target}`,
-        tastingSheet,
-        await getHeaders(currentUser)
-      )
-      setTastingSheet(tastingSheetApi)
+      await client.put(`/tasting_sheets/${target}`, tastingSheet, await getHeaders(currentUser))
     } catch (e) {
       if (e instanceof Error) throw e
     } finally {
       setRequesting(false)
     }
+    showToast('シート名を変更しました')
   }
 
   return {
