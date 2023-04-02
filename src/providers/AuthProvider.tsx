4a688337587365ad7333a5dbb76bed103bb19e55
yuma-matsui <oshino.iiyo@gmail.com>
2023-04-02 19:10:23 +0900

    サインイン後のトースト表示処理をhooksに切り出した

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -12,17 +12,15 @@ import { FC, useCallback, useEffect, useLayoutEffect, useMemo, useState } from '
 import { useAuthState, useDeleteUser, useSignOut } from 'react-firebase-hooks/auth'
 
 import { AuthContext } from '../contexts'
-import { useAxios, useToastContext } from '../hooks'
+import { useAxios, useDisplayToastAfterSignedIn } from '../hooks'
 import { firebaseConfig } from '../lib'
 import { ReactNodeChildren } from '../types'
-import { SIGNED_IN_KEY, TASTING_SHEET_KEY } from '../utils'
 
 const AuthProvider: FC<ReactNodeChildren> = ({ children }) => {
   initializeApp(firebaseConfig)
   const auth = getAuth()
 
   const { client, getHeaders } = useAxios()
-  const { showToast } = useToastContext()
 
   const [currentUser, setCurrentUser] = useState<User | null>(null)
   const [signInLoading, setSignInLoading] = useState(false)
@@ -35,20 +33,13 @@ const AuthProvider: FC<ReactNodeChildren> = ({ children }) => {
   const loading = signInLoading || authChangeLoading || deleteLoading || deleteAccountLoading
   const error = authError || authChangeError || signOutError || deleteError
 
+  useDisplayToastAfterSignedIn(currentUser)
+
   useLayoutEffect(() => {
     onAuthStateChanged(auth, (user) => {
       setCurrentUser(user)
     })
   }, [auth, setCurrentUser])
-
-  useEffect(() => {
-    const justAfterSignedIn =
-      window.localStorage.getItem(SIGNED_IN_KEY) && !window.localStorage.getItem(TASTING_SHEET_KEY)
-    if (currentUser && justAfterSignedIn) {
-      showToast('ログインしました')
-      window.localStorage.clear()
-    }
-  }, [currentUser, showToast])
 
   const signIn = useCallback(() => {
     setSignInLoading(true)
