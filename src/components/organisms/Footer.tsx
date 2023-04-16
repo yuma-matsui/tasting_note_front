ca5a407f1719007d160a104860d943530d22c05f
yuma-matsui <oshino.iiyo@gmail.com>
2023-04-16 10:47:01 +0900

    フッターのボタンをコンポーネントに切り出した

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,16 +1,11 @@
 import { FC, memo } from 'react'
 
-import { useAuthContext, useCheckEditingForm, useOnClickAuth, useOnClickOpenModal } from '../../hooks'
-import { DeleteAccountButton, FooterLink, PrivacyPolicyLink, TermOfServiceLink } from '../atoms'
+import { useAuthContext, useCheckEditingForm } from '../../hooks'
+import { DeleteAccountButton, FooterLink, PrivacyPolicyLink, SignOutButton, TermOfServiceLink } from '../atoms'
 
 const Footer: FC = memo(() => {
   const { currentUser } = useAuthContext()
   const { isEditing } = useCheckEditingForm()
-  const { onClickSignOut } = useOnClickAuth()
-  const { onClickOpenModal } = useOnClickOpenModal({
-    text: '本当に削除してもよろしいですか？',
-    rightButton: <DeleteAccountButton />
-  })
 
   return (
     <footer className="flex flex-col items-center border-t border-gray-500 pt-4 mt-4">
@@ -26,14 +21,10 @@ const Footer: FC = memo(() => {
           {currentUser && (
             <>
               <li>
-                <button type="button" onClick={onClickSignOut}>
-                  ログアウト
-                </button>
+                <SignOutButton />
               </li>
               <li>
-                <button type="button" onClick={onClickOpenModal} className="text-red-700">
-                  アカウント削除
-                </button>
+                <DeleteAccountButton />
               </li>
             </>
           )}
