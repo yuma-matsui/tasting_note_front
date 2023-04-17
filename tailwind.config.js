e46ea38a3fd271be3551d3a0bcc992aeb2406e17
yuma-matsui <oshino.iiyo@gmail.com>
2023-04-17 21:45:08 +0900

    サービスのテーマカラーを追加した

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'theme-red': '#ee6055',
+        'theme-green': '#60d394',
+        'theme-emerald': '#aaf683',
+        'theme-yellow': '#ffd97d',
+        'theme-pink': '#ff9b85'
+      }
+    }
+  },
+  daisyui: {
+    themes: false
   },
   plugins: [require('daisyui')]
 }
