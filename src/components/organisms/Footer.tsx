1a4b8c1b25411a04e1bf8e89b6400136bd46062a
yuma-matsui <oshino.iiyo@gmail.com>
2023-04-22 10:53:28 +0900

    フッターのリンクをナビゲーションコンポーネントに切り出した

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,36 +1,17 @@
 import { FC, memo } from 'react'
 
-import { useAuthContext, useCheckEditingForm } from '../../hooks'
-import { DeleteAccountButton, FooterLink, PrivacyPolicyLink, SignOutButton, TermOfServiceLink } from '../atoms'
+import { useCheckEditingForm } from '../../hooks'
+import { FooterLink } from '../atoms'
+import { FooterNavigation } from '../molecules'
 
 const Footer: FC = memo(() => {
-  const { currentUser } = useAuthContext()
   const { isEditing } = useCheckEditingForm()
 
   return (
-    <footer className="flex flex-col items-center border-t border-gray-500 pt-4 mt-4">
+    <footer>
       <FooterLink />
-      {!isEditing && (
-        <ul className="flex list-none">
-          <li className="mr-4">
-            <TermOfServiceLink />
-          </li>
-          <li>
-            <PrivacyPolicyLink />
-          </li>
-          {currentUser && (
-            <>
-              <li>
-                <SignOutButton />
-              </li>
-              <li>
-                <DeleteAccountButton />
-              </li>
-            </>
-          )}
-        </ul>
-      )}
-      <p>&copy; 2023 yuma-matsui</p>
+      {!isEditing && <FooterNavigation />}
+      <p className="text-gray-700">&copy;2023 yuma-matsui</p>
     </footer>
   )
 })
