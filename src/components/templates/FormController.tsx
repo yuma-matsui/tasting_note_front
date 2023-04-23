a52a28de8a6860aa08f4e26a3ac9cb320d61b405
yuma-matsui <oshino.iiyo@gmail.com>
2023-04-23 20:57:48 +0900

    テイスティグシート新規作成ページ下部のボタンの配置を変更した

--- src/components/templates/FormController.tsx
+++ src/components/templates/FormController.tsx
@@ -3,6 +3,7 @@ import { FC, memo, useRef } from 'react'
 import { useBeforeUnload } from '../../hooks'
 import { FormControllerProps } from '../../types'
 import { ConfirmationAndBackButton, FormControllerButton, PostTastingSheetButton } from '../atoms'
+import useGetButtonsFlexType from '../../hooks/useGetButtonsFlexType'
 
 const FormController: FC<FormControllerProps> = memo(
   ({
@@ -18,30 +19,33 @@ const FormController: FC<FormControllerProps> = memo(
   }) => {
     useBeforeUnload()
     const submitRef = useRef<HTMLInputElement>(null)
+    const { getButtonsFlexType } = useGetButtonsFlexType()
 
     return (
       <>
         {children}
-        {!isFirstStep && !isAppearanceStep && (
-          <FormControllerButton
-            value={backButtonText}
-            disabled={disabled}
-            onClick={() => onClick('back', submitRef)}
-            tastingSheet={tastingSheet}
-          />
-        )}
-        {isAppearanceStep && <ConfirmationAndBackButton tastingSheet={tastingSheet} />}
-        {isLastStep ? (
-          <PostTastingSheetButton tastingSheet={tastingSheet} />
-        ) : (
-          <FormControllerButton
-            value={nextButtonText}
-            disabled={disabled}
-            onClick={() => onClick('next', submitRef)}
-            tastingSheet={tastingSheet}
-          />
-        )}
-        <input type="submit" hidden ref={submitRef} />
+        <div className={`w-full flex ${getButtonsFlexType(isFirstStep)}`}>
+          {!isFirstStep && !isAppearanceStep && (
+            <FormControllerButton
+              value={backButtonText}
+              disabled={disabled}
+              onClick={() => onClick('back', submitRef)}
+              tastingSheet={tastingSheet}
+            />
+          )}
+          {isAppearanceStep && <ConfirmationAndBackButton tastingSheet={tastingSheet} />}
+          {isLastStep ? (
+            <PostTastingSheetButton tastingSheet={tastingSheet} />
+          ) : (
+            <FormControllerButton
+              value={nextButtonText}
+              disabled={disabled}
+              onClick={() => onClick('next', submitRef)}
+              tastingSheet={tastingSheet}
+            />
+          )}
+          <input type="submit" hidden ref={submitRef} />
+        </div>
       </>
     )
   }
