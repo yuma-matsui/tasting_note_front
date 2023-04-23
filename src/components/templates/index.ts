827018241b1fef8e1416a249f081823271018007
yuma-matsui <oshino.iiyo@gmail.com>
2023-04-23 09:11:32 +0900

    不要なコンポーネントを削除した

--- src/components/templates/index.ts
+++ src/components/templates/index.ts
@@ -1,4 +1,3 @@
 export { default as DefaultLayout } from './DefaultLayout'
-export { default as OnlyFooterLayout } from './OnlyFooterLayout'
 export { default as TastingSheetFormWrapper } from './TastingSheetFormWrapper'
 export { default as FormController } from './FormController'
