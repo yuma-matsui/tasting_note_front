83f8a052892ff25698f35cbbc9e83ecaf9aa6fb8
yuma-matsui <oshino.iiyo@gmail.com>
2023-04-23 09:14:50 +0900

    タイトルの文字間隔を広げた

--- src/components/molecules/titles/TopPageTitle.tsx
+++ src/components/molecules/titles/TopPageTitle.tsx
@@ -6,7 +6,7 @@ const TopPageTitle: FC = memo(() => (
   <div className="flex items-center">
     <img className="w-14 h-14" src={logo} alt="logo" />
     <div className="ml-2 flex flex-col">
-      <h1 className="text-theme-red font-bold text-2xl">Tasting Note</h1>
+      <h1 className="text-theme-red font-bold text-2xl tracking-wide">Tasting Note</h1>
       <p className="text-xs mt-1">テイスティングを記録してソムリエに</p>
     </div>
   </div>
