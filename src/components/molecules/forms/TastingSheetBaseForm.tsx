5a45ad1a5334a2e79ba171409a7ed036f195f349
yuma-matsui <oshino.iiyo@gmail.com>
2023-04-23 22:23:56 +0900

    テイスティグシート新規作成ページのフォームコンポーネントにスタイルを当てた

--- src/components/molecules/forms/TastingSheetBaseForm.tsx
+++ src/components/molecules/forms/TastingSheetBaseForm.tsx
@@ -7,19 +7,22 @@ import { TastingSheetCheckBox } from '../../atoms'
 import { TastingSheetFormWrapper } from '../../templates'
 import ConclusionSelectBoxes from '../ConclusionSelectBoxes'
 
-const TastingSheetBaseForm: FC<TastingSheetBaseFormProps> = memo(
-  ({ type, items, options, register, lessThanTwoItems, getValues }) => {
-    const { isDisabled } = useTastingSheetInputsAttributes()
+const TastingSheetBaseForm: FC<TastingSheetBaseFormProps> = memo(({ type, items, options, register, getValues }) => {
+  const { isDisabled } = useTastingSheetInputsAttributes()
 
-    return (
-      <TastingSheetFormWrapper title={type}>
+  return (
+    <TastingSheetFormWrapper title={type}>
+      <div className="w-full mt-4 mb-6 border-black border-2 drop-shadow-md">
         {items.map(({ heading, name, labels, subHeading }) => (
           <div key={heading}>
-            <h3>
+            <h3 className="text-lg font-semibold p-2 bg-gray-300 border-y border-gray-400 box-content">
               {heading}
-              {subHeading && <span>{subHeading}</span>}
+              {subHeading && <span className="ml-2 text-base font-normal">{subHeading}</span>}
+              {subHeading && (
+                <span className="text-slate-500 text-base font-normal block text-right">2つ選択してください</span>
+              )}
             </h3>
-            <div>
+            <div className="flex flex-wrap p-2">
               {labels.map((label) => (
                 <TastingSheetCheckBox
                   key={label}
@@ -32,17 +35,12 @@ const TastingSheetBaseForm: FC<TastingSheetBaseFormProps> = memo(
                 />
               ))}
             </div>
-            {lessThanTwoItems(name) && (
-              <p>
-                <span>2つ選択してください</span>
-              </p>
-            )}
           </div>
         ))}
         {type === 'conclusion' && <ConclusionSelectBoxes register={register} options={options} />}
-      </TastingSheetFormWrapper>
-    )
-  }
-)
+      </div>
+    </TastingSheetFormWrapper>
+  )
+})
 
 export default TastingSheetBaseForm
