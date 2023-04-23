b59364e2627e43c0c1ebcdb8a427d8f223f5d147
yuma-matsui <oshino.iiyo@gmail.com>
2023-04-23 22:27:55 +0900

    不要なクラス名を取り除いた

--- src/components/atoms/selects/TastingSheetSelectBox.tsx
+++ src/components/atoms/selects/TastingSheetSelectBox.tsx
@@ -4,7 +4,7 @@ import { TastingSheetSelectBoxProps } from '../../../types'
 import BaseSelectBox from './BaseSelectBox'
 
 const TastingSheetSelectBox: FC<TastingSheetSelectBoxProps> = memo(({ id, register, name, options, label }) => (
-  <div className="">
+  <div>
     <h3 className="text-lg font-semibold p-2 bg-gray-300 border-y border-gray-400 box-content">{label}</h3>
     <BaseSelectBox id={id} register={register} name={name} options={options} label={label} />
   </div>
