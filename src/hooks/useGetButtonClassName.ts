5f48f73187b62a9c45460ef10b4deaa2f28e350d
yuma-matsui <oshino.iiyo@gmail.com>
2023-04-23 22:21:35 +0900

    テイスティグシート新規作成ページのはじめるボタンの色を変更した

--- src/hooks/useGetButtonClassName.ts
+++ src/hooks/useGetButtonClassName.ts
@@ -3,8 +3,8 @@ import { TastingSheet } from '../types'
 const useGetButtonClassName = ({ color }: TastingSheet, value?: string, disabled = false) => {
   let className = 'base-btn '
   if (color === 'red') className += 'bg-theme-red'
-  if (color === 'white') className += 'bg-theme-green'
-  if (value && value === 'テイスティングをはじめる') className += ' bg-theme-pink'
+  if (value !== 'テイスティングをはじめる' && color === 'white') className += 'bg-theme-green'
+  if (value && value === 'テイスティングをはじめる') className += ' bg-theme-red'
   if (disabled) className += ' opacity-25'
 
   return {
