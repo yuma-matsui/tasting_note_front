e7df0ab3c7fda8cd9cfb949796402b038aeddd35
yuma-matsui <oshino.iiyo@gmail.com>
2023-04-25 21:01:05 +0900

    サービスのサブタイトルの大きさを変更した

--- src/components/molecules/logos/HeaderLogo.tsx
+++ src/components/molecules/logos/HeaderLogo.tsx
@@ -7,7 +7,7 @@ const HeaderLogo: FC = memo(() => (
     <img src={logo} alt="Header Logo" className="w-14 h-14" />
     <div className="ml-2 flex flex-col justify-between">
       <h2 className="text-theme-red text-2xl tracking-wide font-semibold text-left">Tasting Note</h2>
-      <p className="leading-5">テイスティングを記録してソムリエに</p>
+      <p className="text-xs">テイスティングを記録してソムリエに</p>
     </div>
   </div>
 ))
--- src/components/molecules/titles/TopPageTitle.tsx
+++ src/components/molecules/titles/TopPageTitle.tsx
@@ -7,7 +7,7 @@ const TopPageTitle: FC = memo(() => (
     <img className="w-16 h-16" src={logo} alt="logo" />
     <div className="ml-2 flex flex-col justify-between">
       <h1 className="text-theme-red font-bold text-3xl tracking-wider">Tasting Note</h1>
-      <p className="leading-5">テイスティングを記録してソムリエに</p>
+      <p className="text-xs">テイスティングを記録してソムリエに</p>
     </div>
   </div>
 ))
