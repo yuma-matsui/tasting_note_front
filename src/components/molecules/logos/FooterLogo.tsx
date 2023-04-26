a834f220bd1e47207216197f94247c28cfb10bec
yuma-matsui <oshino.iiyo@gmail.com>
2023-04-26 22:04:55 +0900

    ロゴのフォントを変更した

--- src/components/molecules/logos/FooterLogo.tsx
+++ src/components/molecules/logos/FooterLogo.tsx
@@ -5,7 +5,7 @@ import logo from '../../../assets/images/logo.png'
 const FooterLogo: FC = memo(() => (
   <div className="flex">
     <img src={logo} alt="Footer Logo" className="w-6 h-6 mr-1" />
-    <p>Tasting Note</p>
+    <p className="font-mincho">Tasting Note</p>
   </div>
 ))
 
