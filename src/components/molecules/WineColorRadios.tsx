bdfc594734409a0676db3c454291c2d307fb1249
yuma-matsui <oshino.iiyo@gmail.com>
2023-04-26 22:39:11 +0900

    スマホ以上のデザインをタブレットサイズに合わせた

--- src/components/molecules/WineColorRadios.tsx
+++ src/components/molecules/WineColorRadios.tsx
@@ -10,7 +10,7 @@ const WineColorRadios: FC<{
 }> = memo(({ register }) => (
   <div className="flex flex-col items-center w-full mb-6">
     <span className="mb-2">ワインの色</span>
-    <div className="flex justify-between w-1/2">
+    <div className="flex justify-between md:justify-around w-1/2">
       {WINE_COLORS.map((color) => (
         <TastingSheetCheckBox
           key={color}
--- src/components/pages/WelcomePage.tsx
+++ src/components/pages/WelcomePage.tsx
@@ -11,7 +11,7 @@ const WelcomePage: FC = memo(() => (
       <WelcomePageAboutSection />
       <div className="demo-img-wrapper bg-gray-700 text-white mb-4">サービスのデモ画像配置予定</div>
       <WelcomePageWithRegistrationSection />
-      <div className="sub-wrapper flex justify-between">
+      <div className="flex justify-between">
         <StartTastingButton />
         <SignInButton />
       </div>
