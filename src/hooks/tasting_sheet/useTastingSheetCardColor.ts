62a6d108958ad5782d7c31694124483134b79499
yuma-matsui <oshino.iiyo@gmail.com>
2023-04-28 22:59:30 +0900

    カードコンポーネント内の色を変更した

--- src/hooks/tasting_sheet/useTastingSheetCardColor.ts
+++ src/hooks/tasting_sheet/useTastingSheetCardColor.ts
@@ -1,8 +1,8 @@
 import { TastingSheetApi } from '../../types'
 
 const useTastingSheetCardColor = (tastingSheet: TastingSheetApi) => ({
-  bgColor: tastingSheet.color === 'red' ? 'bg-red-600' : 'bg-emerald-500',
-  textColor: tastingSheet.color === 'red' ? 'text-red-700' : 'text-emerald-500'
+  bgColor: tastingSheet.color === 'red' ? 'bg-theme-red' : 'bg-theme-green',
+  textColor: tastingSheet.color === 'red' ? 'text-theme-red' : 'text-theme-green'
 })
 
 export default useTastingSheetCardColor
