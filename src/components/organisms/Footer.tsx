ee251c576d00a2cbef00fd8f387a229920487ac7
yuma-matsui <oshino.iiyo@gmail.com>
2023-04-28 21:17:28 +0900

    編集中ページにもログアウトとアカウント削除のリンクを表示するようにした

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,21 +1,16 @@
 import { FC, memo } from 'react'
 
-import { useCheckEditingForm } from '../../hooks'
 import { FooterLink } from '../atoms'
 import { FooterNavigation } from '../molecules'
 
-const Footer: FC = memo(() => {
-  const { isEditing } = useCheckEditingForm()
-
-  return (
-    <footer className="mt-6 main-wrapper">
-      <div className="flex flex-col items-center pt-4 border-t">
-        <FooterLink />
-        {!isEditing && <FooterNavigation />}
-        <p className={`text-gray-700 ${isEditing ? 'mt-2' : ''}`}>&copy;2023 yuma-matsui</p>
-      </div>
-    </footer>
-  )
-})
+const Footer: FC = memo(() => (
+  <footer className="mt-6 main-wrapper">
+    <div className="flex flex-col items-center pt-4 border-t">
+      <FooterLink />
+      <FooterNavigation />
+      <p className="text-gray-700">&copy;2023 yuma-matsui</p>
+    </div>
+  </footer>
+))
 
 export default Footer
