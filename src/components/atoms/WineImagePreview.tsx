f44ae37ddc895a948df46d44118a1417b0d37c3f
yuma-matsui <oshino.iiyo@gmail.com>
2023-04-29 19:39:45 +0900

    画像の表示サイズのmaxサイズを変更した

--- src/components/atoms/WineImagePreview.tsx
+++ src/components/atoms/WineImagePreview.tsx
@@ -3,7 +3,7 @@ import { FC, memo } from 'react'
 const WineImagePreview: FC<{ imageFile: File }> = memo(({ imageFile }) => {
   const url = window.URL.createObjectURL(imageFile)
 
-  return <img src={url} alt="Wine Preview" className="mb-4" />
+  return <img src={url} alt="Wine Preview" className="mb-4 max-w-md" />
 })
 
 export default WineImagePreview
