8c1d68ac06adda571f12a09ccf2df277e20381e8
yuma-matsui <oshino.iiyo@gmail.com>
2023-04-29 17:49:42 +0900

    ワインフォームの画像表示コンポーネントにスタイルを当てた

--- src/components/atoms/WineImage.tsx
+++ src/components/atoms/WineImage.tsx
@@ -1,7 +1,7 @@
 import { FC, memo } from 'react'
 
 const WineImage: FC<{ filename: string }> = memo(({ filename }) => (
-  <img src={`${process.env.REACT_APP_CF_DOMAIN}/${filename}`} alt="wine" className="w-72 h-72" />
+  <img src={`${process.env.REACT_APP_CF_DOMAIN}/${filename}`} alt="wine" className="mb-4" />
 ))
 
 export default WineImage
