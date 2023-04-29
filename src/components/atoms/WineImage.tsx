1bcdee1a458e37b8ecbb9053ae715d23acf139cf
yuma-matsui <oshino.iiyo@gmail.com>
2023-04-29 19:59:53 +0900

    ワイン詳細表示欄の画像を中央寄せにした

--- src/components/atoms/WineImage.tsx
+++ src/components/atoms/WineImage.tsx
@@ -1,7 +1,7 @@
 import { FC, memo } from 'react'
 
 const WineImage: FC<{ filename: string }> = memo(({ filename }) => (
-  <img src={`${process.env.REACT_APP_CF_DOMAIN}/${filename}`} alt="wine" className="mb-4 max-w-md" />
+  <img src={`${process.env.REACT_APP_CF_DOMAIN}/${filename}`} alt="wine" className="mb-4 max-w-md mx-auto" />
 ))
 
 export default WineImage
