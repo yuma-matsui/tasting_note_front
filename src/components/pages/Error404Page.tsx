d9d9804675fcafcd2f7a73864174befa6f77bdd8
yuma-matsui <oshino.iiyo@gmail.com>
2023-04-29 20:41:52 +0900

    404ページにデザインを当てた

--- src/components/pages/Error404Page.tsx
+++ src/components/pages/Error404Page.tsx
@@ -1,17 +1,17 @@
 import { FC } from 'react'
+import { Link } from 'react-router-dom'
 
 import { DefaultLayout } from '../templates'
-import { GoToAnotherPageButton } from '../atoms'
 
 const Error404Page: FC = () => (
   <DefaultLayout>
-    <div className="flex flex-col items-center">
-      <p className="break-word w-1/3">
-        お探しのページは一時的にアクセスができない状況にあるか、移動もしくは削除された可能性があります。
-        また、URLにタイプミスがないか再度ご確認ください。
-      </p>
-      <GoToAnotherPageButton text="Tasting Noteトップページへ" to="/" />
-    </div>
+    <p className="break-word leading-loose mb-10 md:px-5">
+      お探しのページは一時的にアクセスができない状況にあるか、移動もしくは削除された可能性があります。
+      また、URLにタイプミスがないか再度ご確認ください。
+    </p>
+    <Link to="/" className="w-full block text-center">
+      Tasting Noteトップページへ
+    </Link>
   </DefaultLayout>
 )
 
