31e85526e2931292ddc034c01c6261203a4f21c4
yuma-matsui <oshino.iiyo@gmail.com>
2023-04-29 09:53:54 +0900

    カードコンポーネント内に表示するワインの編集用リンクを共通化した

---
+++ src/components/atoms/links/CardInsideLink.tsx
@@ -0,0 +1,29 @@
+import { FC, memo } from 'react'
+import { Link } from 'react-router-dom'
+
+import { WineApi } from '../../../types'
+import WineColor from '../../../types/tasting_sheet/wineColor'
+
+const CardInsideLink: FC<{
+  text: string
+  to: string
+  textColor: string
+  state:
+    | WineApi
+    | {
+        id: number
+        name: string
+        color: WineColor
+      }
+}> = memo(({ text, to, textColor, state }) => (
+  <p className="absolute top-36 w-full text-center px-2">
+    {text}
+    <span className={`${textColor} text-lg font-bold`}>
+      <Link to={to} state={state}>
+        こちら
+      </Link>
+    </span>
+  </p>
+))
+
+export default CardInsideLink
