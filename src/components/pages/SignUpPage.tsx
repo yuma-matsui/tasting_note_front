5d4ce0176bbae4d760c59f7c773a3819a7ccfb09
yuma-matsui <oshino.iiyo@gmail.com>
2023-05-06 09:17:30 +0900

    サインアップ用のページの雛形を作った

---
+++ src/components/pages/SignUpPage.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react'
+import { DefaultLayout } from '../templates'
+
+const SignUpPage: FC = () => (
+  <DefaultLayout>
+    <h2>サインアップ</h2>
+  </DefaultLayout>
+)
+
+export default SignUpPage
