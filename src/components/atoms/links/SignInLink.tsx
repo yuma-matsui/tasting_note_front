dd40a5da72c6065418ee2240baf1bea40ee2491f
yuma-matsui <oshino.iiyo@gmail.com>
2023-05-06 10:09:01 +0900

    ログイン、サインアップページへ遷移するリンクコンポーネントを作った

--- src/components/atoms/links/SignInLink.tsx
+++ src/components/atoms/links/SignInLink.tsx
@@ -1,11 +1,11 @@
 import { FC, memo } from 'react'
-import { FcGoogle } from 'react-icons/fc'
+import { BiLogInCircle } from 'react-icons/bi'
 import { Link } from 'react-router-dom'
 
 const SignInLink: FC = memo(() => (
-  <Link to="/login" className="base-btn bg-theme-green w-40">
+  <Link to="/signin" className="base-btn bg-theme-green w-44">
     <div className="flex items-center justify-center">
-      <FcGoogle className="mr-1" />
+      <BiLogInCircle className="mr-1" />
       <span>ログイン</span>
     </div>
   </Link>
