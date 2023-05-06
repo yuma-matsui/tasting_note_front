0ed86ab2a47b841d7095c355dadc154e7e86e143
yuma-matsui <oshino.iiyo@gmail.com>
2023-05-06 22:17:41 +0900

    トップページに表示するリンクの横幅を変更した

--- src/components/atoms/links/SignInLink.tsx
+++ src/components/atoms/links/SignInLink.tsx
@@ -3,7 +3,7 @@ import { BiLogInCircle } from 'react-icons/bi'
 import { Link } from 'react-router-dom'
 
 const SignInLink: FC = memo(() => (
-  <Link to="/signin" className="base-btn bg-theme-green w-44">
+  <Link to="/signin" className="base-btn bg-theme-green w-40">
     <div className="flex items-center justify-center">
       <BiLogInCircle className="mr-1" />
       <span>ログイン</span>
--- src/components/atoms/links/SignUpLink.tsx
+++ src/components/atoms/links/SignUpLink.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { MdAppRegistration } from 'react-icons/md'
 
 const SignUpLink: FC = memo(() => (
-  <Link to="/signup" className="base-btn bg-theme-red w-44">
+  <Link to="/signup" className="base-btn bg-theme-red w-40">
     <div className="flex items-center justify-center">
       <MdAppRegistration className="mr-1" />
       <span>サインアップ</span>
