534936d5c3cca22ddcca03ea5659f89a794bf350
yuma-matsui <oshino.iiyo@gmail.com>
2023-05-23 21:07:00 +0900

    FormControllerButtonのテストをリファクタした

--- src/components/atoms/buttons/FormControllerButton.tsx
+++ src/components/atoms/buttons/FormControllerButton.tsx
@@ -1,14 +1,9 @@
 import { FC, memo } from 'react'
 
-import { TastingSheet } from '../../../types'
+import { FormControllerButtonProps } from '../../../types'
 import { useGetButtonClassName } from '../../../hooks'
 
-const FormControllerButton: FC<{
-  value: string
-  disabled: boolean
-  onClick: () => void
-  tastingSheet: TastingSheet
-}> = memo(({ value, disabled, onClick, tastingSheet }) => {
+const FormControllerButton: FC<FormControllerButtonProps> = memo(({ value, disabled, onClick, tastingSheet }) => {
   const { className } = useGetButtonClassName(tastingSheet.color, disabled, value)
 
   return <input type="button" value={value} disabled={disabled} onClick={onClick} className={className} />
