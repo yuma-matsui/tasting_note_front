ce41c45d5cdd68df725fcf5e6f7a338298204c12
yuma-matsui <oshino.iiyo@gmail.com>
2023-05-23 19:30:40 +0900

    CloseModalButtonのテストをリファクタした

--- src/components/atoms/buttons/__test__/CloseModalButton.test.tsx
+++ src/components/atoms/buttons/__test__/CloseModalButton.test.tsx
@@ -3,18 +3,27 @@ import userEvent from '@testing-library/user-event'
 
 import CloseModalButton from '../CloseModalButton'
 
+const setUp = ({ onClick }: { onClick: () => void }) => {
+  const utils = render(<CloseModalButton onClick={onClick} />)
+  const button = screen.getByRole('button')
+
+  return {
+    ...utils,
+    button
+  }
+}
+
 describe('CloseModalButton', () => {
+  const onClickMock = jest.fn() as () => void
+
   it('クリックされた場合、引数に与えた関数が実行される', () => {
-    const onClickMock = jest.fn() as () => void
-
-    render(<CloseModalButton onClick={onClickMock} />)
-    userEvent.click(screen.getByRole('button'))
-
+    const { button } = setUp({ onClick: onClickMock })
+    userEvent.click(button)
     expect(onClickMock).toHaveBeenCalled()
   })
 
   it('"いいえ"が表示される', () => {
-    render(<CloseModalButton onClick={() => {}} />)
-    expect(screen.getByText('いいえ')).toBeInTheDocument()
+    const { getByText } = setUp({ onClick: onClickMock })
+    expect(getByText('いいえ')).toBeInTheDocument()
   })
 })
