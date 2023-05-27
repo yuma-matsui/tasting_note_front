09f2cdc6af2c298935118884a7575b4d52a0076d
yuma-matsui <oshino.iiyo@gmail.com>
2023-05-27 15:27:21 +0900

    CardInsideLinkのテストケースを追加した

--- src/components/atoms/links/__test__/CardInsideLink.test.tsx
+++ src/components/atoms/links/__test__/CardInsideLink.test.tsx
@@ -1,15 +1,25 @@
 import { render } from '@testing-library/react'
-import { MemoryRouter } from 'react-router-dom'
+import { RouterProvider, createMemoryRouter } from 'react-router-dom'
+import userEvent from '@testing-library/user-event'
 
 import { CardInsideLinkProps, WineApi } from '../../../../types'
 import CardInsideLink from '../CardInsideLink'
 
 const setUp = ({ text, to, textColor, state }: CardInsideLinkProps) => {
-  const utils = render(<CardInsideLink text={text} to={to} textColor={textColor} state={state} />, {
-    wrapper: MemoryRouter
-  })
+  const router = createMemoryRouter([
+    {
+      path: '/',
+      element: <CardInsideLink text={text} to={to} textColor={textColor} state={state} />
+    },
+    {
+      path: '/test',
+      element: <p>test</p>
+    }
+  ])
+  const utils = render(<RouterProvider router={router} />)
 
   return {
+    router,
     ...utils
   }
 }
@@ -35,5 +45,24 @@ describe('CardInsideLink', () => {
   test('toを持つaタグが表示される', () => {
     const { getByRole } = setUp(props)
     expect(getByRole('link')).toHaveAttribute('href', '/')
+  })
+
+  test('クリックされるとtoへ遷移する', () => {
+    props.to = '/test'
+    const { router, getByRole } = setUp(props)
+
+    userEvent.click(getByRole('link'))
+    expect(router.state.location.pathname).toEqual(props.to)
+  })
+
+  test('クリックされるとstateが更新される', () => {
+    props.state = {
+      name: 'test'
+    } as WineApi
+
+    const { router, getByRole } = setUp(props)
+    userEvent.click(getByRole('link'))
+
+    expect(router.state.location.state).toBe(props.state)
   })
 })
