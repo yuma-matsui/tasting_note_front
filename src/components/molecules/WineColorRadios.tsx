9084cf949d935aeb01ff5b7f677a95dddfd37256
yuma-matsui <oshino.iiyo@gmail.com>
2023-05-30 21:08:19 +0900

    WineColorRadiosのテストを書いた

--- src/components/molecules/WineColorRadios.tsx
+++ src/components/molecules/WineColorRadios.tsx
@@ -1,13 +1,10 @@
 import { FC, memo } from 'react'
 
-import { UseFormRegister } from 'react-hook-form'
-import { TastingSheetFormState } from '../../types'
 import { WINE_COLORS } from '../../assets'
 import { TastingSheetCheckBox } from '../atoms'
+import { WineColorRadiosProps } from '../../types'
 
-const WineColorRadios: FC<{
-  register: UseFormRegister<TastingSheetFormState>
-}> = memo(({ register }) => (
+const WineColorRadios: FC<WineColorRadiosProps> = memo(({ register }) => (
   <div className="flex flex-col items-center w-full mb-6">
     <span className="mb-2">ワインの色</span>
     <div className="flex justify-between md:justify-around w-1/2">
