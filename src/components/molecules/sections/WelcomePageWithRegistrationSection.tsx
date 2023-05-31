45c2bfa1edb23f1237f82e1a361bed44a4c53902
yuma-matsui <oshino.iiyo@gmail.com>
2023-05-31 15:43:07 +0900

    WithRegistrationSectionに説明文を追加した

--- src/components/molecules/sections/WelcomePageWithRegistrationSection.tsx
+++ src/components/molecules/sections/WelcomePageWithRegistrationSection.tsx
@@ -3,6 +3,7 @@ import { FC, memo } from 'react'
 const WelcomePageWithRegistrationSection: FC = memo(() => (
   <section className="my-5">
     <h2 className="top-page-section-heading">With Registration</h2>
+    <p className="mb-3">登録をすると以下の機能が使えます</p>
     <ul className="mt-2">
       <li className="mt-2">・テイスティングシートの記録・管理</li>
       <li className="mt-2">・記録したテイスティングシートの検索</li>
