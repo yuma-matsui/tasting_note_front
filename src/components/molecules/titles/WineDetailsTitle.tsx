9a3931aa5f76008f472c75a2b38596e1a3af92d6
yuma-matsui <oshino.iiyo@gmail.com>
2023-06-03 10:45:50 +0900

    各フォームの見出しのデザインを修正した

--- src/components/molecules/titles/WineDetailsTitle.tsx
+++ src/components/molecules/titles/WineDetailsTitle.tsx
@@ -13,7 +13,7 @@ const WineDetailsTitle: FC<{ wine: WineApi }> = memo(({ wine }) => {
   return (
     <h2 className="page-title mb-4">
       {wine.name}
-      <span className="text-gray-400 text-sm font-normal ml-2">
+      <span className="text-gray-300 text-sm font-normal ml-2">
         <button type="button" onClick={onClickOpenModal}>
           削除
         </button>
