1a02e8353d0b1e7d21448ae7251586fc3ad46697
yuma-matsui <oshino.iiyo@gmail.com>
2023-06-07 09:34:26 +0900

    EditWinePageのテストを書いた

---
+++ src/components/pages/__test__/EditWinePage.test.tsx
@@ -0,0 +1,37 @@
+import { render } from '@testing-library/react'
+
+import { ReactNodeChildren, WineApi } from '../../../types'
+import EditWinePage from '../EditWinePage'
+
+jest.mock('../../molecules/HeadMeta', () => ({ children }: ReactNodeChildren) => (
+  <>
+    <p>MockedHeadMeta</p>
+    {children}
+  </>
+))
+
+jest.mock('../../molecules/forms/WineForm', () => () => <p>MockedWineForm</p>)
+
+jest.mock('../../templates/DefaultLayout', () => ({ children }: ReactNodeChildren) => (
+  <>
+    <p>MockedDefaultLayout</p>
+    {children}
+  </>
+))
+
+const setUp = (wine: WineApi) => {
+  const utils = render(<EditWinePage wine={wine} />)
+
+  return {
+    ...utils
+  }
+}
+
+describe('EditWinePage', () => {
+  const wine = {} as WineApi
+
+  test.each([['HeadMeta'], ['DefaultLayout'], ['WineForm']])('%sが表示される', (componentName) => {
+    const { getByText } = setUp(wine)
+    expect(getByText(`Mocked${componentName}`)).toBeInTheDocument()
+  })
+})
