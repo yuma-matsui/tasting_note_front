8d561d0bec5bdbb2528fdad4021c88535db087c6
yuma-matsui <oshino.iiyo@gmail.com>
2023-06-10 10:16:28 +0900

    useCheckCheckBoxStatusのテストを書いた

---
+++ src/hooks/__test__/useCheckCheckBoxStatus.test.ts
@@ -0,0 +1,50 @@
+import { renderHook } from '@testing-library/react'
+
+import { WineColor } from '../../types'
+import useCheckCheckBoxStatus from '../useCheckCheckBoxStatus'
+
+const setUp = (type: string, color: WineColor) => {
+  const { result } = renderHook(() => useCheckCheckBoxStatus(type, color))
+
+  return {
+    ...result.current
+  }
+}
+
+type TestCases = [WineColor, boolean][]
+
+describe('useCheckCheckBoxStatus', () => {
+  describe('isCheckBox', () => {
+    test.each([
+      ['checkbox', true],
+      ['radio', false]
+    ])('typeが%sの場合、%pが返る', (type, result) => {
+      const { isCheckBox } = setUp(type, 'red')
+      expect(isCheckBox).toBe(result)
+    })
+  })
+
+  describe('isRed', () => {
+    const testCases: TestCases = [
+      ['red', true],
+      ['white', false]
+    ]
+
+    test.each(testCases)('colorが%sの場合、%pが返る', (color, result) => {
+      const { isRed } = setUp('radio', color)
+      expect(isRed).toBe(result)
+    })
+  })
+
+  describe('isWhite', () => {
+    const testCases: TestCases = [
+      ['red', false],
+      ['white', true]
+    ]
+
+    test.each(testCases)('colorが%sの場合、%pが返る', (color, result) => {
+      const { isWhite } = setUp('radio', color)
+      expect(isWhite).toBe(result)
+    })
+  })
+})
