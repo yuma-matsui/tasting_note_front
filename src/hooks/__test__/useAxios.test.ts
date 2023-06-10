d18c99b5f7fa7310a4561c9f2101e33a84de447d
yuma-matsui <oshino.iiyo@gmail.com>
2023-06-10 09:49:09 +0900

    useAxiosのテストを書いた

---
+++ src/hooks/__test__/useAxios.test.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+
+import { renderHook } from '@testing-library/react'
+import { User } from 'firebase/auth'
+
+import useAxios from '../useAxios'
+import { client } from '../../lib'
+
+describe('useAxios', () => {
+  test('client', () => {
+    const { result } = renderHook(() => useAxios())
+    expect(result.current.client).toBe(client)
+  })
+
+  test('getHeaders', async () => {
+    const mockedUser = {
+      getIdToken: () => {}
+    } as User
+
+    const { result } = renderHook(() => useAxios())
+    expect((await result.current.getHeaders(mockedUser)).headers.Authorization.includes('Bearer')).toBeTruthy()
+  })
+})
