ca94cb8d74e550cf7fe440b800e228d79cd21eb7
yuma-matsui <oshino.iiyo@gmail.com>
2023-06-10 10:27:20 +0900

    useCheckEditingFormのテストを書いた

---
+++ src/hooks/__test__/useCheckEditingForm.test.ts
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+
+import { renderHook } from '@testing-library/react'
+import Router from 'react-router-dom'
+
+import useCheckEditingForm from '../useCheckEditingForm'
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useLocation: jest.fn()
+}))
+
+const setUp = () => {
+  const { result } = renderHook(() => useCheckEditingForm())
+
+  return {
+    ...result.current
+  }
+}
+
+describe('useCheckEditingForm', () => {
+  describe('isEditing', () => {
+    test.each([
+      ['/tasting_sheets/new', true],
+      ['/wines', true],
+      ['/tasting_sheets/1', false],
+      ['/pp', false]
+    ])('useLocationで取得したpathnameが$sの場合、%pを返す', (pathname, result) => {
+      jest.spyOn(Router, 'useLocation').mockReturnValue({
+        pathname,
+        state: null,
+        key: '',
+        search: '',
+        hash: ''
+      })
+
+      const { isEditing } = setUp()
+      expect(isEditing).toBe(result)
+    })
+  })
+})
