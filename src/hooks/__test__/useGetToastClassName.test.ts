c15994f5f0a4090092b7866e90b661e6e818ba82
yuma-matsui <oshino.iiyo@gmail.com>
2023-06-10 16:46:34 +0900

    useGetToastClassNameのテストを書いた

---
+++ src/hooks/__test__/useGetToastClassName.test.ts
@@ -0,0 +1,34 @@
+import { renderHook } from '@testing-library/react'
+
+import { ToastType } from '../../types'
+import useGetToastClassName from '../useGetToastClassName'
+
+const setUp = (type: ToastType) => {
+  const { result } = renderHook(() => useGetToastClassName(type))
+
+  return {
+    ...result.current
+  }
+}
+
+type TestCases = [ToastType, string][]
+
+describe('useGetToastClassName', () => {
+  const testCases: TestCases = [
+    ['success', 'alert-success'],
+    ['error', 'alert-error'],
+    ['warning', 'alert-warning']
+  ]
+
+  describe('toastColorClass', () => {
+    test('alertが含まれている', () => {
+      const { toastColorClass } = setUp('success')
+      expect(toastColorClass.includes('alert')).toBeTruthy()
+    })
+
+    test.each(testCases)('typeが%sの場合、%sが含まれている', (type, result) => {
+      const { toastColorClass } = setUp(type)
+      expect(toastColorClass.includes(result)).toBeTruthy()
+    })
+  })
+})
