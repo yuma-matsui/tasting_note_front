47e3f4ca9e2bad848f59717b7d27f29216f2798e
yuma-matsui <oshino.iiyo@gmail.com>
2023-06-17 09:48:11 +0900

    useGetIsMultipleInputsのテストを書いた

--- src/hooks/tasting_sheet/__test__/useGetIsMultipleInputs.test.ts
+++ src/hooks/tasting_sheet/__test__/useGetIsMultipleInputs.test.ts
@@ -5,24 +5,22 @@ import useGetIsMultipleInputs from '../useGetIsMultipleInputs'
 describe('useGetIsMultipleInputs', () => {
   describe('isMultipleInputs', () => {
     test.each([
-      ['appearanceColors', true],
-      ['appearanceImpressions', true],
-      ['flavorFirstImpressions', true],
-      ['flavorFruits', true],
-      ['flavorFlowers', true],
-      ['flavorSpices', true],
-      ['flavorImpressions', true]
-    ])('nameが%sの場合は%pを返す', (name, result) => {
+      ['appearanceColors'],
+      ['appearanceImpressions'],
+      ['flavorFirstImpressions'],
+      ['flavorFruits'],
+      ['flavorFlowers'],
+      ['flavorSpices'],
+      ['flavorImpressions']
+    ])('nameが%sの場合はtrueを返す', (name) => {
       const {
         result: { current }
       } = renderHook(() => useGetIsMultipleInputs())
-
-      expect(current.isMultipleInputs(name)).toEqual(result)
+      expect(current.isMultipleInputs(name)).toBeTruthy()
     })
 
     test.each([['test'], ['brightness'], ['consistency']])('それ以外の場合はfalseを返す', (name) => {
       const { result } = renderHook(() => useGetIsMultipleInputs())
-
       expect(result.current.isMultipleInputs(name)).toBeFalsy()
     })
   })
