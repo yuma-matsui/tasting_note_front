3a90401457a343b7c10a84ca5bc7226f8282aef9
yuma-matsui <oshino.iiyo@gmail.com>
2023-06-17 10:46:00 +0900

    useTastingSheetTimerのテストをリファクタリングした

--- src/hooks/tasting_sheet/__test__/useTastingSheetTimer.test.ts
+++ src/hooks/tasting_sheet/__test__/useTastingSheetTimer.test.ts
@@ -1,8 +1,9 @@
 import React from 'react'
 import { cleanup, renderHook } from '@testing-library/react'
 
-import { TastingSheet } from '../../../types'
 import useTastingSheetTimer from '../useTastingSheetTimer'
+import { TastingSheet } from '../../../types'
+import { initialTastingSheet } from '../../../utils'
 
 const mockShowToast = jest.fn()
 jest.mock('../../context/useToastContext', () => () => ({
@@ -10,6 +11,7 @@ jest.mock('../../context/useToastContext', () => () => ({
 }))
 
 const SIXTY_SECONDS = 60
+const calculateHalfTime = (stringMinutes: string) => (Number(stringMinutes) * SIXTY_SECONDS) / 2
 
 describe('useTastingSheetTimer', () => {
   let tastingSheet: TastingSheet
@@ -18,41 +20,52 @@ describe('useTastingSheetTimer', () => {
   const mockClearInterval = jest.spyOn(window, 'clearInterval')
   const mockUseState = jest.spyOn(React, 'useState')
 
+  let secondTimer: number
+  let isJustBeforeTimeUp: boolean
+
   const mockSetIsJustBeforeTimeUp = jest.fn()
   const mockSetSecondTimer = jest.fn()
 
   beforeEach(() => {
-    tastingSheet = { time: '5' } as TastingSheet
+    secondTimer = 0
+    isJustBeforeTimeUp = false
+    tastingSheet = { ...initialTastingSheet, time: '5' }
     mockUseState.mockRestore()
   })
 
-  test('実行時にsetSecondTimerが呼ばれる', () => {
-    jest
-      .spyOn(React, 'useState')
-      .mockReturnValueOnce([false, mockSetIsJustBeforeTimeUp])
-      .mockReturnValueOnce([0, mockSetSecondTimer])
-
-    renderHook(() => useTastingSheetTimer(tastingSheet))
-    expect(mockSetSecondTimer).toHaveBeenCalled()
-  })
-
-  test('実行時にsetIntervalが呼ばれる', () => {
-    renderHook(() => useTastingSheetTimer(tastingSheet))
-    expect(mockSetInterval).toHaveBeenCalledWith(expect.any(Function), 1000)
-  })
-
-  test('unmount時にclearIntervalが呼ばれる', () => {
-    renderHook(() => useTastingSheetTimer(tastingSheet))
-    cleanup()
-    expect(mockClearInterval).toHaveBeenCalled()
-  })
-
-  describe('secondTimer === 0、isJustBeforeTimeUpがtrueの場合', () => {
+  describe('useEffect', () => {
     beforeEach(() => {
       jest
         .spyOn(React, 'useState')
-        .mockReturnValueOnce([true, mockSetIsJustBeforeTimeUp])
-        .mockReturnValueOnce([0, mockSetSecondTimer])
+        .mockReturnValueOnce([isJustBeforeTimeUp, mockSetIsJustBeforeTimeUp])
+        .mockReturnValueOnce([secondTimer, mockSetSecondTimer])
+    })
+
+    test('実行時にsetSecondTimerが呼ばれる', () => {
+      renderHook(() => useTastingSheetTimer(tastingSheet))
+      expect(mockSetSecondTimer).toHaveBeenCalled()
+    })
+
+    test('実行時にsetIntervalが呼ばれる', () => {
+      renderHook(() => useTastingSheetTimer(tastingSheet))
+      expect(mockSetInterval).toHaveBeenCalledWith(expect.any(Function), 1000)
+    })
+
+    test('unmount時にclearIntervalが呼ばれる', () => {
+      renderHook(() => useTastingSheetTimer(tastingSheet))
+      cleanup()
+      expect(mockClearInterval).toHaveBeenCalled()
+    })
+  })
+
+  describe('secondTimerが0、isJustBeforeTimeUpがtrueの場合', () => {
+    beforeEach(() => {
+      secondTimer = 0
+      isJustBeforeTimeUp = true
+      jest
+        .spyOn(React, 'useState')
+        .mockReturnValueOnce([isJustBeforeTimeUp, mockSetIsJustBeforeTimeUp])
+        .mockReturnValueOnce([secondTimer, mockSetSecondTimer])
     })
 
     test('showToastが呼ばれる', () => {
@@ -69,77 +82,99 @@ describe('useTastingSheetTimer', () => {
     })
   })
 
-  test('secondTimerが60以下で0でない場合、showToastが呼ばれる', () => {
-    jest
-      .spyOn(React, 'useState')
-      .mockReturnValueOnce([true, mockSetIsJustBeforeTimeUp])
-      .mockReturnValueOnce([60, mockSetSecondTimer])
+  describe('secondTimerが60以下で0でない場合', () => {
+    beforeEach(() => {
+      secondTimer = 60
+      jest
+        .spyOn(React, 'useState')
+        .mockReturnValueOnce([true, mockSetIsJustBeforeTimeUp])
+        .mockReturnValueOnce([60, mockSetSecondTimer])
+    })
 
-    renderHook(() => useTastingSheetTimer(tastingSheet))
-    expect(mockShowToast).toHaveBeenCalledWith({
-      text: '残り時間1分です',
-      type: 'warning'
+    test('showToastが呼ばれる', () => {
+      renderHook(() => useTastingSheetTimer(tastingSheet))
+      expect(mockShowToast).toHaveBeenCalledWith({
+        text: '残り時間1分です',
+        type: 'warning'
+      })
     })
   })
 
-  test('secondTimerが引数に与えたtastingSheet.timeの半分の秒数で0でない場合、showToastが呼ばれる', () => {
-    const halfTime = (Number(tastingSheet.time) * SIXTY_SECONDS) / 2
+  describe('secondTimerが引数tastingSheetのtimeの半分の秒数 && 0でない場合', () => {
+    beforeEach(() => {
+      secondTimer = calculateHalfTime(tastingSheet.time)
+      jest
+        .spyOn(React, 'useState')
+        .mockReturnValueOnce([isJustBeforeTimeUp, mockSetIsJustBeforeTimeUp])
+        .mockReturnValueOnce([secondTimer, mockSetSecondTimer])
+    })
 
-    jest
-      .spyOn(React, 'useState')
-      .mockReturnValueOnce([true, mockSetIsJustBeforeTimeUp])
-      .mockReturnValueOnce([halfTime, mockSetSecondTimer])
-
-    renderHook(() => useTastingSheetTimer(tastingSheet))
-    expect(mockShowToast).toHaveBeenCalledWith({
-      text: '残り時間半分です',
-      type: 'warning'
+    test('showToastが呼ばれる', () => {
+      renderHook(() => useTastingSheetTimer(tastingSheet))
+      expect(mockShowToast).toHaveBeenCalledWith({
+        text: '残り時間半分です',
+        type: 'warning'
+      })
     })
   })
 
-  test('secondTimerが1の場合、setIsJustBeforeTimeUpが呼ばれる', () => {
-    jest
-      .spyOn(React, 'useState')
-      .mockReturnValueOnce([true, mockSetIsJustBeforeTimeUp])
-      .mockReturnValueOnce([1, mockSetSecondTimer])
+  describe('secondTimerが1の場合', () => {
+    beforeEach(() => {
+      secondTimer = 1
+      jest
+        .spyOn(React, 'useState')
+        .mockReturnValueOnce([isJustBeforeTimeUp, mockSetIsJustBeforeTimeUp])
+        .mockReturnValueOnce([secondTimer, mockSetSecondTimer])
+    })
 
-    renderHook(() => useTastingSheetTimer(tastingSheet))
-    expect(mockSetIsJustBeforeTimeUp).toHaveBeenCalledWith(true)
+    test('setIsJustBeforeTimeUpが呼ばれる', () => {
+      renderHook(() => useTastingSheetTimer(tastingSheet))
+      expect(mockSetIsJustBeforeTimeUp).toHaveBeenCalledWith(true)
+    })
   })
 
   describe('timerClassName', () => {
     describe('secondTimerが60以下の場合', () => {
-      test('text-theme-redが返る', () => {
+      beforeEach(() => {
+        secondTimer = 60
         jest
           .spyOn(React, 'useState')
-          .mockReturnValueOnce([false, mockSetIsJustBeforeTimeUp])
-          .mockReturnValueOnce([30, mockSetSecondTimer])
+          .mockReturnValueOnce([isJustBeforeTimeUp, mockSetIsJustBeforeTimeUp])
+          .mockReturnValueOnce([secondTimer, mockSetSecondTimer])
+      })
 
+      test('text-theme-redが返る', () => {
         const { result } = renderHook(() => useTastingSheetTimer(tastingSheet))
         expect(result.current.timerClassName).toEqual('text-theme-red')
       })
     })
 
     describe('secondTimerが0、isJustBeforeTimeUpがtrueの場合', () => {
-      test('test-theme-redが返る', () => {
+      beforeEach(() => {
+        secondTimer = 0
+        isJustBeforeTimeUp = true
         jest
           .spyOn(React, 'useState')
-          .mockReturnValueOnce([true, mockSetIsJustBeforeTimeUp])
-          .mockReturnValueOnce([0, mockSetSecondTimer])
+          .mockReturnValueOnce([isJustBeforeTimeUp, mockSetIsJustBeforeTimeUp])
+          .mockReturnValueOnce([secondTimer, mockSetSecondTimer])
+      })
 
+      test('test-theme-redが返る', () => {
         const { result } = renderHook(() => useTastingSheetTimer(tastingSheet))
         expect(result.current.timerClassName).toEqual('text-theme-red')
       })
     })
 
-    describe('secondTimerが引数に与えたtastingSheet.timeの半分の秒数で0でない場合', () => {
-      test('test-theme-yellowが返る', () => {
-        const halfTime = (Number(tastingSheet.time) * SIXTY_SECONDS) / 2
+    describe('secondTimerが引数tastingSheetのtimeの半分の秒数で0でない場合', () => {
+      beforeEach(() => {
+        secondTimer = calculateHalfTime(tastingSheet.time)
         jest
           .spyOn(React, 'useState')
-          .mockReturnValueOnce([true, mockSetIsJustBeforeTimeUp])
-          .mockReturnValueOnce([halfTime, mockSetSecondTimer])
+          .mockReturnValueOnce([isJustBeforeTimeUp, mockSetIsJustBeforeTimeUp])
+          .mockReturnValueOnce([secondTimer, mockSetSecondTimer])
+      })
 
+      test('test-theme-yellowが返る', () => {
         const { result } = renderHook(() => useTastingSheetTimer(tastingSheet))
         expect(result.current.timerClassName).toEqual('text-theme-yellow')
       })
@@ -154,11 +189,12 @@ describe('useTastingSheetTimer', () => {
   })
 
   describe('styleForMinute', () => {
+    beforeEach(() => {})
     test('{ "--value": Number(残り分数) } のオブジェクトが返る', () => {
       const leftSecond = 299
       jest
         .spyOn(React, 'useState')
-        .mockReturnValueOnce([false, mockSetIsJustBeforeTimeUp])
+        .mockReturnValueOnce([isJustBeforeTimeUp, mockSetIsJustBeforeTimeUp])
         .mockReturnValueOnce([leftSecond, mockSetSecondTimer])
 
       const { result } = renderHook(() => useTastingSheetTimer(tastingSheet))
@@ -173,7 +209,7 @@ describe('useTastingSheetTimer', () => {
       const leftSecond = 300
       jest
         .spyOn(React, 'useState')
-        .mockReturnValueOnce([false, mockSetIsJustBeforeTimeUp])
+        .mockReturnValueOnce([isJustBeforeTimeUp, mockSetIsJustBeforeTimeUp])
         .mockReturnValueOnce([leftSecond, mockSetSecondTimer])
 
       const { result } = renderHook(() => useTastingSheetTimer(tastingSheet))
