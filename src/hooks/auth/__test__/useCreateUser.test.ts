50627a3d44d1e5e028122a7b0a241dbb81b786be
yuma-matsui <oshino.iiyo@gmail.com>
2023-06-18 09:44:26 +0900

    useCreateUserのテストを書いた

---
+++ src/hooks/auth/__test__/useCreateUser.test.ts
@@ -0,0 +1,63 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+
+import ErrorBoundary from 'react-error-boundary'
+import { User } from 'firebase/auth'
+import { renderHook } from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
+
+import mockUseAxios from '../../useAxios'
+import useCreateUser from '../useCreateUser'
+
+jest.mock('react-error-boundary', () => ({
+  ...jest.requireActual('react-error-boundary'),
+  useErrorBoundary: jest.fn()
+}))
+
+jest.mock('../../useAxios')
+
+describe('useCreateUser', () => {
+  const user = {
+    uid: 'test',
+    email: 'test@example.com'
+  } as User
+
+  const mockClient = {
+    post: jest.fn()
+  }
+  const mockGetHeaders = jest.fn()
+  const mockHeaders = {
+    headers: {
+      Authorization: 'Bearer test'
+    }
+  }
+
+  beforeEach(() => {
+    jest.spyOn(ErrorBoundary, 'useErrorBoundary').mockReturnValue({
+      ...jest.requireActual('react-error-boundary'),
+      showBoundary: jest.fn()
+    })
+
+    mockGetHeaders.mockImplementation(() => mockHeaders)
+    ;(mockUseAxios as jest.Mock).mockImplementation(() => ({
+      client: mockClient,
+      getHeaders: mockGetHeaders
+    }))
+  })
+
+  describe('createUser', () => {
+    test('client.postが呼ばれる', async () => {
+      const { result } = renderHook(() => useCreateUser())
+      await act(() => result.current.createUser(user))
+      expect(mockClient.post).toHaveBeenCalledWith(
+        '/users',
+        {
+          user: {
+            uid: user.uid,
+            email: user.email
+          }
+        },
+        mockHeaders
+      )
+    })
+  })
+})
