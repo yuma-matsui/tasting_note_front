d7512f0c14986b7f6346cd3cc764c99b16adb0af
yuma-matsui <oshino.iiyo@gmail.com>
2023-06-22 22:26:14 +0900

    E2Eテスト用のテストデータを用意した

---
+++ src/utils/test_data/conclusion.ts
@@ -0,0 +1,13 @@
+import { Conclusion } from '../../types'
+
+const conclusion: Conclusion = {
+  evaluation: '成熟度が高く豊か',
+  optimumTemperature: '8度未満',
+  glass: '小ぶり',
+  vintage: '2020',
+  country: 'イタリア',
+  grape: 'シャルドネ',
+  decantage: null
+}
+
+export default conclusion
