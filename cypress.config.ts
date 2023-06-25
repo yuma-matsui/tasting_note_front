ed5c6ae33132dc943008d375692868c57e46f115
yuma-matsui <oshino.iiyo@gmail.com>
2023-06-25 16:59:20 +0900

    cypress実行時にprocess.envがundefinedにならないようにした

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from 'cypress'
+import 'dotenv/config'
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
-      process.env.REACT_APP_FIREBASE_API_KEY = process.env.REACT_APP_FIREBASE_API_KEY
-      process.env.REACT_APP_FIREBASE_AUTH_DOMAIN = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN
-      process.env.REACT_APP_FIREBASE_PROJECT_ID = process.env.REACT_APP_FIREBASE_PROJECT_ID
-      process.env.REACT_APP_FIREBASE_STORAGE_BUCKET = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
-      process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID = process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
-      process.env.REACT_APP_FIREBASE_APP_ID = process.env.REACT_APP_FIREBASE_APP_ID
-      process.env.REACT_APP_FIREBASE_MEASUREMENT_ID = process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+      config.env = process.env
+      return config
     }
   },
   video: false
