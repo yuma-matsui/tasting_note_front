a7f7fa677377c9838bbf86d8d108df2c45b8b5a9
yuma-matsui <oshino.iiyo@gmail.com>
2023-06-25 09:35:53 +0900

    cypress内でprocess.envを使用できるようにした

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,15 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    setupNodeEvents(on, config) {}
-  }
+    setupNodeEvents(on, config) {
+      process.env.REACT_APP_FIREBASE_API_KEY = process.env.REACT_APP_FIREBASE_API_KEY
+      process.env.REACT_APP_FIREBASE_AUTH_DOMAIN = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN
+      process.env.REACT_APP_FIREBASE_PROJECT_ID = process.env.REACT_APP_FIREBASE_PROJECT_ID
+      process.env.REACT_APP_FIREBASE_STORAGE_BUCKET = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
+      process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID = process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+      process.env.REACT_APP_FIREBASE_APP_ID = process.env.REACT_APP_FIREBASE_APP_ID
+      process.env.REACT_APP_FIREBASE_MEASUREMENT_ID = process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    }
+  },
+  video: false
 })
