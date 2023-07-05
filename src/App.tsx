02bf440ea4f2b25a9303b6e177041e4bf2d1d9cc
yuma-matsui <oshino.iiyo@gmail.com>
2023-07-05 09:30:39 +0900

    Errorfallbackの呼び出し位置を変更した

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { FC } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
 import { HelmetProvider } from 'react-helmet-async'
 
 import RouterConfig from './router/RouterConfig'
 import { AuthProvider, RequestingProvider, ToastProvider } from './providers'
+import { ErrorFallbackForApi } from './components/pages'
 
 const App: FC = () => (
   <HelmetProvider>
     <ToastProvider>
       <AuthProvider>
-        <RequestingProvider>
-          <RouterConfig />
-        </RequestingProvider>
+        <ErrorBoundary FallbackComponent={ErrorFallbackForApi}>
+          <RequestingProvider>
+            <RouterConfig />
+          </RequestingProvider>
+        </ErrorBoundary>
       </AuthProvider>
     </ToastProvider>
   </HelmetProvider>
